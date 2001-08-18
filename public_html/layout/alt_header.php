2346059ad64cba6afde2e84fc04e73170bab5827
tanepiper <devnull@localhost>
2001-08-18 16:48:04 +0000

    Fixed the contact and geeklog sitename bug

--- public_html/layout/alt_header.php
+++ public_html/layout/alt_header.php
@@ -8,7 +8,7 @@
 
 <table border=0 cellspacing=0 cellpadding=0 width="100%">
 <tr align=right valign=middle><td height=18 colspan=2><a href=<?php echo $CONF["base"]; ?>>home</a>
- / <a href=<?php echo "{$CONF["base"]}/users.php?mode=profile&uid=1"; ?>>contact</a>&nbsp;</td></tr>
+ / <a href=mailto:<?php echo $CONF["sitemail"]; ?>><?php print $LANG01[69];?></a>&nbsp;</td></tr>
 <tr valign=bottom><td>&nbsp;<a href=<?php echo $CONF["base"]; ?>><img src=<?php echo $CONF["base"]; ?>/images/logo.gif width=350 height=84 border=0 alt=<?php echo $CONF["sitename"] ?>></a></td>
 <td align=right><form action="<?php echo $CONF["base"]; ?>/search.php" method=get>search<input type=text size=15 name="query">&nbsp;</form></td></tr>
 <tr><td bgcolor=000000 colspan=2><img src=<?php echo $CONF["base"]; ?>/images/speck.gif width=1 height=1></td></tr>
@@ -30,7 +30,7 @@
 
 <table border=0 cellspacing=0 cellpadding=0 width="100%">
 <tr><td bgcolor=FFFFFF colspan=2><img src=<?php echo $CONF["base"]; ?>/images/speck.gif width=1 height=1></td></tr>
-<tr bgcolor=DDDDDD><td height=20>&nbsp;<b>Welcome to GeekLog
+<tr bgcolor=DDDDDD><td height=20>&nbsp;<b><?php print $LANG01[67] . $CONF["sitename"]; ?>
 <?php if (!empty($USER["name"])) echo " {$USER["name"]}"; ?>!</b></td><td align=right><b><?php echo date("D M j"); ?></b>&nbsp;</td></tr>
 <tr><td bgcolor=AAAAAA colspan=2><img src=<?php echo $CONF["base"]; ?>/images/speck.gif width=1 height=1></td></tr>
 <tr><td bgcolor=FFFFFF colspan=2><img src=<?php echo $CONF["base"]; ?>/images/speck.gif width=1 height=15></td></tr>
