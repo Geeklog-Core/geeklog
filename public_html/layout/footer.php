3e6b0699475f44bf7249d4a60d301ee8f30b4df1
tony_bibbs <devnull@localhost>
2001-09-11 21:48:36 +0000

    Added code for the "Created this page in X seconds" hack.  Thanks to phpBB group for this code

--- public_html/layout/footer.php
+++ public_html/layout/footer.php
@@ -9,7 +9,17 @@
 <tr><td bgcolor=EEEEEE colspan=2><img src=<?php echo $CONF["base"]; ?>/images/speck.gif width=1 height=1></td></tr>
 <tr bgcolor=DDDDDD valign=top><td height=20 class=footer>&nbsp;Copyright &copy; 2000 <?php echo $CONF["sitename"];?><br>
 &nbsp;All trademarks and copyrights on this page are owned by their respective owners.</td>
-<td align=right class=footer>Powered By: <a href=http://www.geeklog.org>GeekLog v<?php echo $VERSION;?></a></td></tr>
+<td align=right class=footer>Powered By: <a href=http://www.geeklog.org>GeekLog v<?php echo $VERSION;?></a>
+<?php
+     	$mtime = microtime();
+     	$mtime = explode(" ",$mtime);
+     	$mtime = $mtime[1] + $mtime[0];
+    	$endtime = $mtime;
+	$totaltime = sprintf("%.2f",$endtime - $starttime);
+     	$howfast = $LANG01[91] . ' ' . $totaltime . ' ' . $LANG01[92];
+	print '<br>' . $howfast;
+?>
+</td></tr>
 <tr><td bgcolor=AAAAAA colspan=2><img src=<?php echo $CONF["base"]; ?>/images/speck.gif width=1 height=1></td></tr>
 </table>
 
