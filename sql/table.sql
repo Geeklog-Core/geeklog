ef811602019796ed31addb7fbb12ff73454837cf
tony_bibbs <devnull@localhost>
2001-09-13 14:35:51 +0000

    Added phpblockfn to block table.  That field is used for PHP in block support

--- sql/table.sql
+++ sql/table.sql
@@ -17,6 +17,7 @@ CREATE TABLE blocks (
   rdfurl varchar(96),
   rdfupdated datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
   content text,
+  phpblockfn varchar(64) DEFAULT '',
   PRIMARY KEY (bid)
 );
 
