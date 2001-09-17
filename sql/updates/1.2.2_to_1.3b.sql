36a6875d2f4fd139e7d9b1b0b18a0c1bf3732d38
tony_bibbs <devnull@localhost>
2001-09-17 20:17:01 +0000

    Added new data necessary for block code to work

--- sql/updates/1.2.2_to_1.3b.sql
+++ sql/updates/1.2.2_to_1.3b.sql
@@ -7,5 +7,13 @@ CREATE TABLE plugins(
 	PRIMARY KEY(pi_name)
 );
 ALTER TABLE blocks add phpblockfn varchar(64) DEFAULT '';
-ALTER TABLE blocks add onleft tinyint(3) unsigned DEFAULT '1' NOT NULL;
+ALTER TABLE blocks add onleft tinyint(3) unsigned DEFAULT '0' NOT NULL;
 
+#Mandatory Data for blocks
+INSERT INTO blocks (title,tid,seclev,blockorder,type,rdfurl,rdfupdated,content,onleft,phpblockfn) VALUES ('blockheader','all',255,0,'layout','','0000-00-00 00:00:00','<table border=0 cellpadding=1 cellspacing=0 width=\"100%\"><tr bgcolor=666666><td>\r\n<table width=\"100%\" border=0 cellspacing=0 cellpadding=2>\r\n<tr bgcolor=666666><td class=blocktitle>%title</td><td align=right>%help</td></tr>\r\n<tr><td bgcolor=FFFFFF colspan=2>',1,'');
+INSERT INTO blocks (title,tid,seclev,blockorder,type,rdfurl,rdfupdated,content,onleft,phpblockfn) VALUES ('blockfooter','all',255,0,'layout','','0000-00-00 00:00:00','</td></tr></table>\r\n</td></tr></table><br>',1,'');
+INSERT INTO blocks (title,tid,seclev,blockorder,type,rdfurl,rdfupdated,content,onleft,phpblockfn) VALUES ('Events Block','all',255,2,'gldefault','','0000-00-00 00:00:00','',1,'');
+INSERT INTO blocks (title,tid,seclev,blockorder,type,rdfurl,rdfupdated,content,onleft,phpblockfn) VALUES ('Section Block','all',255,0,'gldefault','','0000-00-00 00:00:00','',1,'');
+INSERT INTO blocks (title,tid,seclev,blockorder,type,rdfurl,rdfupdated,content,onleft,phpblockfn) VALUES ('Poll Block','all',255,2,'gldefault','','0000-00-00 00:00:00','',0,'');
+INSERT INTO blocks (title,tid,seclev,blockorder,type,rdfurl,rdfupdated,content,onleft,phpblockfn) VALUES ('User Block','all',255,1,'gldefault','','0000-00-00 00:00:00','',1,'');
+INSERT INTO blocks (title,tid,seclev,blockorder,type,rdfurl,rdfupdated,content,onleft,phpblockfn) VALUES ('Whats New Block','all',255,3,'gldefault','','0000-00-00 00:00:00','',1,'');
