b26e06b7a4f17d32227d14ae7e23e6b86c2ea48c
tony_bibbs <devnull@localhost>
2001-09-19 14:23:02 +0000

    Added some new fields

--- sql/updates/1.2.2_to_1.3b.sql
+++ sql/updates/1.2.2_to_1.3b.sql
@@ -6,8 +6,12 @@ CREATE TABLE plugins(
 	pi_homepage varchar(128) DEFAULT '' NOT NULL,
 	PRIMARY KEY(pi_name)
 );
+
 ALTER TABLE blocks add phpblockfn varchar(64) DEFAULT '';
 ALTER TABLE blocks add onleft tinyint(3) unsigned DEFAULT '0' NOT NULL;
+ALTER TABLE stories add numemails mediumint(8) unsigned DEFAULT '0' NOT NULL;
+ALTER TABLE users add regdate datetime DEFAULT '0000-00-00 00:00:00' NOT NULL;
+UPDATE users SET regdate = NOW();
 
 #Mandatory Data for blocks
 INSERT INTO blocks (title,tid,seclev,blockorder,type,rdfurl,rdfupdated,content,onleft,phpblockfn) VALUES ('blockheader','all',255,0,'layout','','0000-00-00 00:00:00','<table border=0 cellpadding=1 cellspacing=0 width=\"100%\"><tr bgcolor=666666><td>\r\n<table width=\"100%\" border=0 cellspacing=0 cellpadding=2>\r\n<tr bgcolor=666666><td class=blocktitle>%title</td><td align=right>%help</td></tr>\r\n<tr><td bgcolor=FFFFFF colspan=2>',1,'');
