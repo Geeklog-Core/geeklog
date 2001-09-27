a6405099d1af35ebc5c804103f2d11f5432acd2a
tony_bibbs <devnull@localhost>
2001-09-27 14:18:22 +0000

    Moved a few records to data.sql

--- sql/table.sql
+++ sql/table.sql
@@ -302,26 +302,10 @@ INSERT INTO group_assignments VALUES (7,NULL,1);
 INSERT INTO group_assignments VALUES (8,NULL,1);
 INSERT INTO group_assignments VALUES (9,NULL,1);
 INSERT INTO group_assignments VALUES (10,NULL,1);
-INSERT INTO group_assignments VALUES (7,9,NULL);
 INSERT INTO group_assignments VALUES (11,NULL,1);
 INSERT INTO group_assignments VALUES (1,2,NULL);
 INSERT INTO group_assignments VALUES (13,2,NULL);
-INSERT INTO group_assignments VALUES (8,5,NULL);
-INSERT INTO group_assignments VALUES (5,8,NULL);
-INSERT INTO group_assignments VALUES (6,6,NULL);
-INSERT INTO group_assignments VALUES (9,10,NULL);
-INSERT INTO group_assignments VALUES (12,11,NULL);
-INSERT INTO group_assignments VALUES (3,12,NULL);
 INSERT INTO group_assignments VALUES (2,2,NULL);
-INSERT INTO group_assignments VALUES (3,3,NULL);
-INSERT INTO group_assignments VALUES (2,5,NULL);
-INSERT INTO group_assignments VALUES (2,6,NULL);
-INSERT INTO group_assignments VALUES (4,7,NULL);
-INSERT INTO group_assignments VALUES (2,8,NULL);
-INSERT INTO group_assignments VALUES (2,9,NULL);
-INSERT INTO group_assignments VALUES (2,10,NULL);
-INSERT INTO group_assignments VALUES (2,11,NULL);
-INSERT INTO group_assignments VALUES (2,12,NULL);
 INSERT INTO group_assignments VALUES (2,NULL,12);
 INSERT INTO group_assignments VALUES (2,NULL,10);
 INSERT INTO group_assignments VALUES (2,NULL,9);
@@ -332,19 +316,8 @@ INSERT INTO group_assignments VALUES (2,NULL,5);
 INSERT INTO group_assignments VALUES (2,NULL,4);
 INSERT INTO group_assignments VALUES (2,NULL,3);
 INSERT INTO group_assignments VALUES (12,NULL,1);
-INSERT INTO group_assignments VALUES (2,3,NULL);
-INSERT INTO group_assignments VALUES (2,7,NULL);
 INSERT INTO group_assignments VALUES (9,NULL,11);
 INSERT INTO group_assignments VALUES (2,NULL,11);
-INSERT INTO group_assignments VALUES (13,3,NULL);
-INSERT INTO group_assignments VALUES (13,5,NULL);
-INSERT INTO group_assignments VALUES (13,6,NULL);
-INSERT INTO group_assignments VALUES (13,7,NULL);
-INSERT INTO group_assignments VALUES (13,8,NULL);
-INSERT INTO group_assignments VALUES (13,9,NULL);
-INSERT INTO group_assignments VALUES (13,10,NULL);
-INSERT INTO group_assignments VALUES (13,11,NULL);
-INSERT INTO group_assignments VALUES (13,12,NULL);
 
 #
 # Table structure for table 'groups'
@@ -924,17 +897,7 @@ CREATE TABLE users (
 #
 
 INSERT INTO users VALUES (1,'Anonymous','Anonymous','',NULL,NULL,'','2001-09-18 13:58:56');
-INSERT INTO users VALUES (3,'StoryAdmin','Story Admin','5f4dcc3b5aa765d61d8327deb882cf99','root','http://geeklog.sourceforge.net','','0000-00-00 00:00:00');
-INSERT INTO users VALUES (9,'EventAdmin','Event Administrator','5f4dcc3b5aa765d61d8327deb882cf99','root','','','0000-00-00 00:00:00');
 INSERT INTO users VALUES (2,'Admin','Geeklog SuperUser','5f4dcc3b5aa765d61d8327deb882cf99','root@localhost.com','http://www.geeklog.org','','0000-00-00 00:00:00');
-INSERT INTO users VALUES (5,'PollAdmin','Poll Administrator','5f4dcc3b5aa765d61d8327deb882cf99','root','','','0000-00-00 00:00:00');
-INSERT INTO users VALUES (8,'LinkAdmin','Link Administrator','5f4dcc3b5aa765d61d8327deb882cf99','root','','','0000-00-00 00:00:00');
-INSERT INTO users VALUES (6,'TopicAdmin','Topic Administrator','5f4dcc3b5aa765d61d8327deb882cf99','root','','','0000-00-00 00:00:00');
-INSERT INTO users VALUES (7,'BlockAdmin','Block Administrator','5f4dcc3b5aa765d61d8327deb882cf99','root','','','0000-00-00 00:00:00');
-INSERT INTO users VALUES (10,'UserAdmin','User Administrator','5f4dcc3b5aa765d61d8327deb882cf99','root','','','0000-00-00 00:00:00');
-INSERT INTO users VALUES (11,'MailAdmin','Mail Administrator','5f4dcc3b5aa765d61d8327deb882cf99','root','','','0000-00-00 00:00:00');
-INSERT INTO users VALUES (12,'StoryAdmin2','2nd Story Admin so you can test','5f4dcc3b5aa765d61d8327deb882cf99','root','','','0000-00-00 00:00:00');
-INSERT INTO users VALUES (13,'GroupAdmin','Group Administrator','5f4dcc3b5aa765d61d8327deb882cf99','root','','','0000-00-00 00:00:00');
 
 #
 # Table structure for table 'vars'
