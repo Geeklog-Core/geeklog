7e541a97a17eb2a08d63f0dffba237dfcf131717
tony_bibbs <devnull@localhost>
2001-12-14 16:06:49 +0000

    Modified to use new functions in lib-common.php

--- public_html/404.php
+++ public_html/404.php
@@ -1,9 +1,14 @@
 <?php
-include('lib-common.php');
-site_header("menu");
-startblock("404 Error");
-print "Gee, I've looked everywhere but I can not find <b>http://{$HTTP_SERVER_VARS["HTTP_HOST"]}{$HTTP_SERVER_VARS["REQUEST_URI"]}</b>.";
-print "<p>We're sorry, but the file you have requested does not exist. Please feel free to check the <a href={$CONF['site_url']}>main page</a> or the <a href={$CONF['site_url']}/search.php>search page</a> to see if you can find what you lost.";
-endblock();
-site_footer();
+
+include_once('lib-common.php');
+
+$display = COM_siteHeader('menu');
+$display .= COM_startBlock('404 Error');
+$display .= "Gee, I've looked everywhere but I can not find <b>http://{$HTTP_SERVER_VARS["HTTP_HOST"]}{$HTTP_SERVER_VARS["REQUEST_URI"]}</b>.";
+$display .= "<p>We're sorry, but the file you have requested does not exist. Please feel free to check the <a href={$_CONF['site_url']}>main page</a> or the <a href={$_CONF['site_url']}/search.php>search page</a> to see if you can find what you lost.";
+$display .= COM_endBlock();
+$display .= COM_siteFooter();
+
+echo $display
+
 ?>
