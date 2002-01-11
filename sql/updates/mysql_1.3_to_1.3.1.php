456c5580d3b7496e025708b32787ab27a1e51564
tony_bibbs <devnull@localhost>
2002-01-11 22:45:25 +0000

    minor database updates needed for upgrade from 1.3 to 1.3.1

---
+++ sql/updates/mysql_1.3_to_1.3.1.php
@@ -0,0 +1,7 @@
+<?php
+
+$_SQL[] = "INSERT INTO {$_TABLES['blocks']} (name, type, title, tid, blockorder, content, rdfurl, rdfupdated, onleft, phpblockfn, group_id, owner_id, perm_owner, perm_group, perm_members, perm_anon) VALUES ('whosonline_block','phpblock','Who\'s Online','all',0,'','','0000-00-00 00:00:00',0,'phpblock_whosonline',4,2,3,3,2,2) ";
+$_SQL[] = "CREATE INDEX stories_tid ON stories(tid)";
+$_SQL[] = "CREATE INDEX group_assignments_ug_grp_id ON group_assignments(ug_grp_id)"; 
+
+?>
