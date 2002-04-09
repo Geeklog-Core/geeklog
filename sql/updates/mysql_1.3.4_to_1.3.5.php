88fb8930a7712910c85e40153caa9fe98eb27f5b
tony_bibbs <devnull@localhost>
2002-04-09 19:10:56 +0000

    Added is_enabled flag to blocks table and added ariticle_images table

--- sql/updates/mysql_1.3.4_to_1.3.5.php
+++ sql/updates/mysql_1.3.4_to_1.3.5.php
@@ -1,3 +1,12 @@
 <?php
 $_SQL[] = "ALTER TABLE {$_TABLES['userprefs']} ADD emailfromadmin tinyint(1) NOT NULL DEFAULT '1'";
+$_SQL[] = "ALTER TABLE {$_TABLES['blocks']} ADD is_enabled tinyint(1) unsigned NOT NULL DEFAULT '1'";
+$_SQL[] = "
+CREATE TABLE {$_TABLES['article_images']} (
+  ai_sid varchar(20) NOT NULL,
+  ai_img_num tinyint(2) unsigned NOT NULL,
+  ai_filename varchar(128) NOT NULL,
+  PRIMARY KEY (ai_sid,ai_img_num)
+) TYPE=MyISAM
+";
 ?>
