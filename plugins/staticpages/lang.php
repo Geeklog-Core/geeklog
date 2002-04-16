a61d8ec8264874f600f64e8b65b5fe1bd7a0d3b3
tony_bibbs <devnull@localhost>
2002-04-16 21:07:14 +0000

    adding core staticpages plugin stuff to project

---
+++ plugins/staticpages/lang.php
@@ -0,0 +1,76 @@
+<?php
+
+###############################################################################
+# lang.php
+# This is the english language page for the Geeklog Static Page Plug-in!
+#
+# Copyright (C) 2001 Tony Bibbs
+# tony@tonybibbs.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+###############################################################################
+
+###############################################################################
+# Array Format: 
+# $LANGXX[YY]:	$LANG - variable name
+#		  	XX - file id number
+#			YY - phrase id number
+###############################################################################
+
+
+$LANG_STATIC= array(
+	newpage => "New Page",
+	adminhome => "Admin Home",
+	staticpages => "Static Pages",
+	staticpageeditor => "Static Page Editor",
+	writtenby => "Written By",
+	date => "Last Updated",
+	title => "Title",
+	content => "Content",
+	hits => "Hits",
+	staticpagelist => "Static Page List",
+	usealtheader => "Use Alt. Header",
+	url => "URL",
+	edit => "Edit",
+	lastupdated => "Last Updated",
+	pageformat => "Page Format",
+	leftrightblocks => "Left & Right Blocks",
+	blankpage => "Blank Page",
+	noblocks => "No Blocks",
+	leftblocks => "Left Blocks",
+    addtomenu => 'Add To Menu',
+    label => 'Label',
+    nopages => 'No static pages are in the system yet',
+    save => 'save',
+    preview => 'preview',
+    delete => 'delete',
+    cancel => 'cancel',
+    access_denied => 'Access Denied',
+    access_denied_msg => 'You are illegally trying access one of the Static Pages administration pages.  Please note that all attempts to illegally access this page are logged',
+    installation_complete => 'Installation Complete',
+    installation_complete_msg => 'The data structures for the Static Pages plugin for Geeklog have been successfully installed into your database!  If you ever need to uninstall this plugin, please read the README document that came with this plugin.',
+    installation_failed => 'Installation Failed',
+    installation_failed_msg => 'The installation of the Static Pages plugin failed.  Please see your Geeklog error.log file for diagnostic information',
+    system_locked => 'System Locked',
+    system_locked_msg => 'The Static Pages plugin has already been installed and is locked.  If you are trying to uninstall this plugin, please read the README document that shipped with this plugin',
+    uninstall_complete => 'Uninstall Complete',
+    uninstall_complete_msg => 'The datastructures for the Static Pages plugin have been successfully removed from your Geeklog database.',
+    uninstall_failed => 'Uninstall Failed',
+    uninstall_failed_msg => 'The uninstall of the Static Pages plugin failed.  Please see your Geeklog error.log file for diagnostic information',
+    all_html_allowed => 'All HTML is allowed'
+);
+
+?>
