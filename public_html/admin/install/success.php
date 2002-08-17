d69c124d2a34cbd5153ad84ad5f09f612b5c4de6
dhaun <devnull@localhost>
2002-08-17 20:12:17 +0000

    Attempts at improving the "out-of-the box" security of fresh installations - or at least hinting at how to secure a Geeklog site. Updated some other information along the way.

--- public_html/admin/install/success.php
+++ public_html/admin/install/success.php
@@ -3,12 +3,14 @@
 require_once('../../lib-common.php');
 
 $display = COM_siteHeader();
+$display .= "<h2>Installation of Geeklog $VERSION complete!</h2>";
 if (DB_count($_TABLES['users'],'username','NewAdmin') > 0) {
-    $display .= "Installation of Geeklog $VERSION complete! Because the security model has been changed, we have created a new account with the rights you need to administer your new site.  The username for this new account is <b>NewAdmin</b> and the password is <b>password</b>. Click <a href={$_CONF['site_url']}>here</a> to see your new Geeklog site! <font color=\"red\"><P><b>WARNING:</b></font>Before you do anything else you need to make sure that {$_CONF['path_html']}admin/install/install.php can not be executed by the webserver otherwise someone could do damage to your new Geeklog installation.  Either change the file permissions so it can't be executed OR move the file someplace outside your web tree.";
+    $display .= "Because the security model has been changed, we have created a new account with the rights you need to administer your new site.  The username for this new account is <b>NewAdmin</b> and the password is <b>password</b>. <a href=\"{$_CONF['site_url']}\">Click here</a> to see your new Geeklog site!";
 } else {
-    $display .= "Installation of Geeklog $VERSION complete! Click <a href=\"{$_CONF['site_url']}\">here</a> to see your new Geeklog site! <font color=\"red\"><P><b>WARNING:</b></font>Before you do anything else you need to make sure that {$_CONF['path_html']}admin/install/install.php can not be executed by the webserver otherwise someone could do damage to your new Geeklog installation.  Either change the file permissions so it can't be executed OR move the file someplace outside your web tree.";
+    $display .= "<a href=\"{$_CONF['site_url']}\">Click here</a> to see your new Geeklog site!";
 }
-$display .= "<P><B>NOTE:</B> As of Geeklog 1.3.5, we include the Static Pages Plug-in for Geeklog by default.  If you just completed a fresh installation, you don't need to do anything to start using the Static Pages Plug-in.  If you just upgraded Geeklog, you can activate the Static Pages Plug-in by going <a href=\"{$_CONF['site_admin_url']}/plugins/staticpages/install.php\">here</a>. If you do not want to use this plugin you can disable it from the plugin administration page OR you can follow the directions in /path/to/geeklog/plugins/staticpages/INSTALL";
+$display .= "<p><b><font color=\"red\">IMPORTANT:</font></b> Once you have your site up and running without any errors, do not forget to remove the install directory <b>{$_CONF['path_html']}admin/install</b>. Otherwise, malicious users could seriously damage your site.";
+$display .= "<p><b>NOTE:</b> As of Geeklog 1.3.5, we include the Static Pages Plug-in for Geeklog by default.  If you just completed a fresh installation, you don't need to do anything to start using the Static Pages Plug-in.  If you just upgraded Geeklog, you can activate the Static Pages Plug-in by going <a href=\"{$_CONF['site_admin_url']}/plugins/staticpages/install.php\">here</a>. If you do not want to use this plugin you can disable it from the plugin administration page OR you can follow the directions in {$_CONF['path']}plugins/staticpages/README";
 $display .= COM_siteFooter();
 
 echo $display;
--- system/lib-custom.php
+++ system/lib-custom.php
@@ -38,7 +38,7 @@
 // |                                                                           |
 // +---------------------------------------------------------------------------+
 //
-// $Id: lib-custom.php,v 1.3 2002/07/29 12:18:17 dhaun Exp $
+// $Id: lib-custom.php,v 1.4 2002/08/17 20:12:17 dhaun Exp $
 
 // You can use this global variable to print useful messages to the errorlog
 // using COM_errorLog().  To see an example of how to do this, look in
@@ -48,26 +48,75 @@ $_CST_VERBOSE = false;
 /**
 * Sample PHP Block function
 *
-* this is a sample function used by a PHP block.  This will show the rights that 
+* this is a sample function used by a PHP block.  This will show the rights that
 * a user has in the "What you have access to" block.
 *
 */
 function phpblock_showrights() {
     global $_RIGHTS, $_CST_VERBOSE;
-	
+
     if ($_CST_VERBOSE) {
         COM_errorLog('**** Inside phpblock_showrights in lib-custom.php ****', 1);
     }
-	
+
     $retval .= '&nbsp;';
 
     for ($i = 0; $i < count($_RIGHTS); $i++) {
         $retval .=  '<li>' . $_RIGHTS[$i] . '</li>' . LB;
     }
-	
+
     if ($_CST_VERBOSE) {
         COM_errorLog('**** Leaving phpblock_showrights in lib-custom.php ****', 1);
     }
+
+    return $retval;
+}
+
+
+/***
+*
+* Get Bent()
+*
+* Php function to tell you how if your site is grossly insecure
+* 
+**/
+function phpblock_getBent()
+{
+    global $_CONF, $_TABLES;
+    $secure = true;
+
+    $retval = '';
+
+    $secure_msg = 'Could not find any gross insecurities in your site.  Do not take this ';
+    $secure_msg .= 'as meaning your site is 100% secure, as no site ever is.  I can only ';
+    $secure_msg .= 'check things that should be blatantly obvious.';
+
+    $insecure_msg = '';
+
+    $installdir = $_CONF['path_html'] . 'admin/install';
+
+    if (is_dir ($installdir)) {
+        $insecure_msg .= '<p>You should really remove the install directory <b>' . $installdir .'</b> once you have your site up and running without any errors.';
+        $insecure_msg .= ' Keeping it around would allow malicious users the ability to destroy your current install, take over your site, or retrieve sensitive information.';
+
+        $secure = false;
+    }
+
+    //check to see if any Admin still has 'password' as it's password.
+    $count = DB_query("select count(*) as count from {$_TABLES['users']} where passwd='" . md5('password') . "'");
+    $A = DB_fetchArray($count);
+    if ( $A['count'] > 0 ) {
+        $secure = false;
+        $insecure_msg .= '<p>You still have not changed some default Admin password from "password". ';
+        $insecure_msg .= 'This will allow people to do serious harm to your site!';
+    }
+
+    if ($secure) {
+        $retval = $secure_msg;
+    } else {
+        $retval = $insecure_msg;
+    }
+    $retval = wordwrap($retval,20,' ',1);
 
     return $retval;
 }
