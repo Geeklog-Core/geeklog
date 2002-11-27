ba63db12d044b6789b216aa84d4c60540226ee90
dhaun <devnull@localhost>
2002-11-27 19:41:39 +0000

    Spanish language file for the Static Pages plugin, provided by gorka.

---
+++ plugins/staticpages/language/spanish.php
@@ -0,0 +1,78 @@
+<?php
+
+###############################################################################
+# lang.php
+# This is the spanish language page for the Geeklog Static Page Plug-in!
+#
+# Copyright (C) 2001 Tony Bibbs
+# tony@tonybibbs.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+###############################################################################
+
+###############################################################################
+# Array Format: 
+# $LANGXX[YY]:	$LANG - variable name
+#		  	XX - file id number
+#			YY - phrase id number
+###############################################################################
+
+
+$LANG_STATIC= array(
+	newpage => "Nueva Página",
+	adminhome => "Administración",
+	staticpages => "Páginas Estáticas",
+	staticpageeditor => "Editor de páginas estáticas",
+	writtenby => "Escrito por",
+	date => "Última edición",
+	title => "Título",
+	content => "Contenido",
+	hits => "Hits",
+	staticpagelist => "Lista de Páginas Estáticas",
+	usealtheader => "Usar cabecera alternativa",
+	url => "URL",
+	edit => "Editar",
+	lastupdated => "Última Edición",
+	pageformat => "Formato de Página",
+	leftrightblocks => "Cajas a Derecha e Izquierda",
+	blankpage => "Página en blanco",
+	noblocks => "Sin Cajas",
+	leftblocks => "Cajas a Izquierda",
+    addtomenu => 'Añadir al menú',
+    label => 'Etiqueta',
+    nopages => 'Todavía no hay páginas estáticas',
+    save => 'guardar',
+    preview => 'vista previa',
+    delete => 'eliminar',
+    cancel => 'cancelar',
+    access_denied => 'Acceso Denegado',
+    access_denied_msg => 'Estás intentando acceder a una página de administración de Páginas Estáticas. Ten en cuenta que cualquier acceso a esta página se registra',
+    installation_complete => 'Instalación Completada',
+    installation_complete_msg => 'Las estructuras de datos necesarias para las Páginas Estáticas de Geeklog se han creado correctamente en la base de datos. Si necesitas desinstalar este plugin por favor lee el fichero README',
+    installation_failed => 'Instalación Fallida',
+    installation_failed_msg => 'La instalación del plugin de Páginas Estáticas falló. Por favor lee la información de diagnóstico del fichero error.log de Geeklog.',
+    system_locked => 'Sistema Bloqueado',
+    system_locked_msg => 'El plugin de Páginas Estáticas ya ha sido instalado y está bloqueado. Si estás intentando desinstalar este plugin, por favor, léete el documento README que acompaña al plugin',
+    uninstall_complete => 'Desintalación completada',
+    uninstall_complete_msg => 'Las estructuras de datos para el plugin de Páginas Estáticas han sido eliminadas correctamente de tu base de datos de Geeklog.',
+    uninstall_failed => 'Desinstalación Fallida',
+    uninstall_failed_msg => 'El proceso de desinstalación del plugin de Páginas Estáticas falló. Por favor, lee la información de diagnóstico del fichero error.log de Geeklog',
+    all_html_allowed => 'Se permite cualquier etiqueta HTML',
+    results => 'Resultado de Páginas Estáticas',
+    author => 'Autor'
+);
+
+?>
