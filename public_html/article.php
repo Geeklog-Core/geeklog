c0d238cf90ca36075f70f8f5e45b7b52cf4b4974
dhaun <devnull@localhost>
2003-01-20 19:10:44 +0000

    What's Related block is now created dynamically, removed some redundant code.

--- public_html/article.php
+++ public_html/article.php
@@ -30,7 +30,7 @@
 // |                                                                           |
 // +---------------------------------------------------------------------------+
 //
-// $Id: article.php,v 1.30 2003/01/16 22:52:40 tony Exp $
+// $Id: article.php,v 1.31 2003/01/20 19:10:44 dhaun Exp $
 
 /**
 * This page is responsible for showing a single article in different modes which
@@ -49,7 +49,7 @@ require_once('lib-common.php');
 // to the script.  This will sometimes cause errors but it will allow you to see
 // the data being passed in a POST operation
 
-// debug($HTTP_POST_VARS);
+// echo COM_debug($HTTP_POST_VARS);
 
 // MAIN
 
@@ -71,17 +71,20 @@ $A = DB_fetchArray($result);
 if ($A['count'] > 0) {
     if ($reply == $LANG01[25]) {
         echo COM_refresh($_CONF['site_url'] . "/comment.php?sid=$story&amp;pid=$pid&amp;type=$type");
-    } else if (($mode == "print") && ($_CONF['hideprintericon'] == 0)) {
-        $result = DB_query("SELECT *,unix_timestamp(date) AS day FROM {$_TABLES['stories']} WHERE sid = '$story'");
-        $A = DB_fetchArray($result);
-        $access = SEC_hasAccess($A['owner_id'],$A['group_id'],$A['perm_owner'],$A['perm_group'],$A['perm_members'],$A['perm_anon']);
-        if (SEC_hasAccess($A['owner_id'],$A['group_id'],$A['perm_owner'],$A['perm_group'],$A['perm_members'],$A['perm_anon']) == 0 OR (!SEC_hasTopicAccess($A['tid']))) {   
-            $display .= COM_siteHeader('menu')
-                .COM_startBlock($LANG_ACCESS[accessdenied])
-                .$LANG_ACCESS[storydenialmsg]
-                .COM_endBlock()
-                .COM_siteFooter();
-        } else {
+    } else {
+        $result = DB_query ("SELECT sid,uid,tid,title,introtext,bodytext,hits,comments,featured,show_topic_icon,commentcode,postmode,owner_id,group_id,perm_owner,perm_group,perm_members,perm_anon,unix_timestamp(date) AS day FROM {$_TABLES['stories']} WHERE sid = '$story'");
+        $A = DB_fetchArray ($result);
+
+        $access = SEC_hasAccess ($A['owner_id'], $A['group_id'],
+                $A['perm_owner'], $A['perm_group'], $A['perm_members'],
+                $A['perm_anon']);
+        if (($access == 0) OR !SEC_hasTopicAccess ($A['tid'])) {
+            $display .= COM_siteHeader ('menu')
+                     . COM_startBlock ($LANG_ACCESS['accessdenied'])
+                     . $LANG_ACCESS['storydenialmsg']
+                     . COM_endBlock ()
+                     . COM_siteFooter ();
+        } elseif (($mode == "print") && ($_CONF['hideprintericon'] == 0)) {
             $story_template = new Template($_CONF['path_layout'] . 'article');
             $story_template->set_file('article','printable.thtml');
             $story_template->set_var('page_title',$_CONF['site_name'] . ': ' . stripslashes($A['title'])); 
@@ -102,97 +105,79 @@ if ($A['count'] > 0) {
             $story_template->set_var('lang_comments', $LANG01[3]);
             $story_template->parse('output','article');
             $display = $story_template->finish($story_template->get_var('output')); 
-        }
-    } else {
-        // Set page title
+        } else {
+            // Set page title
+            $_CONF['pagetitle'] = stripslashes (str_replace ('$', '&#36;',
+                                                $A['title']));
+            $display .= COM_siteHeader ('menu');
 
-        $result = DB_query("SELECT *,unix_timestamp(date) AS day FROM {$_TABLES["stories"]} WHERE sid = '$story'");
-        $A = DB_fetchArray($result);
-        $_CONF['pagetitle'] = stripslashes(str_replace('$','&#36;',$A['title']));
-        $display .= COM_siteHeader('menu');
+            DB_change ($_TABLES['stories'], 'hits', DB_getItem ($_TABLES['stories'], 'hits', "sid = '$story'") + 1, 'sid', $story);
 
-        // Make sure user has access to this article
-        $access = SEC_hasAccess($A['owner_id'],$A['group_id'],$A['perm_owner'],$A['perm_group'],$A['perm_members'],$A['perm_anon']);
-        if (SEC_hasAccess($A['owner_id'],$A['group_id'],$A['perm_owner'],$A['perm_group'],$A['perm_members'],$A['perm_anon']) == 0 OR (!SEC_hasTopicAccess($A['tid']))) {
-            // Bail, they don't have access
-            $display .= COM_startBlock($LANG_ACCESS[accessdenied])
-                .$LANG_ACCESS[storydenialmsg]
-                .COM_endBlock()
-                .COM_siteFooter();
-            echo $display;
-            exit;
-        }
-
-        // They have access...go ahead and show the article
-
-        DB_change($_TABLES['stories'],'hits',DB_getItem($_TABLES['stories'],'hits',"sid = '$story'") + 1,'sid',$story);
-        $sql = "SELECT *,unix_timestamp(date) AS day FROM {$_TABLES['stories']} WHERE sid = '$story' ";
-        $result = DB_query($sql);
-        $A = DB_fetchArray($result);
-
-		if($query)
-		{
-			$mywords = explode(" ",$query);
-            foreach ($mywords as $searchword)
-            {
-                    $A['introtext'] = preg_replace ("/($searchword)/i", "<span class=highlight>\\1</span>", $A['introtext']);
-                    $A['bodytext'] = preg_replace ("/($searchword)/i", "<span class=highlight>\\1</span>", $A['bodytext']);
+            if ($query) {
+                $mywords = explode(" ",$query);
+                foreach ($mywords as $searchword) {
+                    $A['introtext'] = preg_replace ("/($searchword)/i",
+                        "<span class=\"highlight\">\\1</span>", $A['introtext']);
+                    $A['bodytext'] = preg_replace ("/($searchword)/i",
+                        "<span class=\"highlight\">\\1</span>", $A['bodytext']);
+                }
             }
 
-		}
+            // Display whats related any polls configured for this page
 
+            $story_template = new Template($_CONF['path_layout'] . 'article');
+            $story_template->set_file('article','article.thtml');
 
-        // Display whats related any polls configured for this page
-
-        $story_template = new Template($_CONF['path_layout'] . 'article');
-        $story_template->set_file('article','article.thtml');
-
-        $story_template->set_var('site_url', $_CONF['site_url']);
-        $story_template->set_var('layout_url', $_CONF['layout_url']);
-        $story_options = array ();
-        if ($_CONF['hideemailicon'] == 0) {
-            $story_options[] = '<a href="' . $_CONF['site_url'] .
-                    '/profiles.php?sid=' . $story . '&amp;what=emailstory">' .
-                    $LANG11[2] . '</a>';
-        }
-        if ($_CONF['hideprintericon'] == 0) {
-            $story_options[] = '<a href="' . $_CONF['site_url']
-                    .  '/article.php?story=' . $story .  '&amp;mode=print">'
+            $story_template->set_var('site_url', $_CONF['site_url']);
+            $story_template->set_var('layout_url', $_CONF['layout_url']);
+            $story_options = array ();
+            if ($_CONF['hideemailicon'] == 0) {
+                $story_options[] = '<a href="' . $_CONF['site_url']
+                    . '/profiles.php?sid=' . $story . '&amp;what=emailstory">'
+                    . $LANG11[2] . '</a>';
+            }
+            if ($_CONF['hideprintericon'] == 0) {
+                $story_options[] = '<a href="' . $_CONF['site_url']
+                    . '/article.php?story=' . $story .  '&amp;mode=print">'
                     . $LANG11[3] . '</a>';
-        }
-        $related = $A['related'];
-        if (!empty ($related)) {
-            $related = COM_startBlock ($LANG11[1], '',
-                COM_getBlockTemplate ('whats_related_block', 'header'))
-                . $A['related']
-                . COM_endBlock (COM_getBlockTemplate ('whats_related_block',
-                    'footer'));
-        }
-        if (count ($story_options) > 0) {
-            $optionsblock = COM_startBlock ($LANG11[4], '',
-                    COM_getBlockTemplate ('story_options_block', 'header'))
-                . COM_makeList ($story_options)
-                . COM_endBlock(COM_getBlockTemplate('story_options_block','footer'));
-        } else {
-            $optionsblock = '';
-        }
-        $story_template->set_var ('whats_related', $related);
-        $story_template->set_var ('story_options', $optionsblock);
-        $story_template->set_var ('whats_related_story_options',
-            $related . $optionsblock);
+            }
+            $related = COM_whatsRelated ($A['introtext'] . ' ' . $A['bodytext'],
+                                         $A['uid'], $A['tid']);
+            if (!empty ($related)) {
+                $related = COM_startBlock ($LANG11[1], '',
+                    COM_getBlockTemplate ('whats_related_block', 'header'))
+                    . $related
+                    . COM_endBlock (COM_getBlockTemplate ('whats_related_block',
+                        'footer'));
+            }
+            if (count ($story_options) > 0) {
+                $optionsblock = COM_startBlock ($LANG11[4], '',
+                        COM_getBlockTemplate ('story_options_block', 'header'))
+                    . COM_makeList ($story_options)
+                    . COM_endBlock (COM_getBlockTemplate ('story_options_block',
+                        'footer'));
+            } else {
+                $optionsblock = '';
+            }
+            $story_template->set_var ('whats_related', $related);
+            $story_template->set_var ('story_options', $optionsblock);
+            $story_template->set_var ('whats_related_story_options',
+                    $related . $optionsblock);
 
-        // if (DB_count($_TABLES['pollquestions'],'qid',$story) > 0) {
-        //  $display .= COM_showPoll(80,$story);
-        // }
-        
-        $story_template->set_var('formatted_article', COM_article($A,'n'));
-        // Display the comments, if there are any ..
-        if ($A['commentcode'] >= 0) {
-                $story_template->set_var('commentbar', COM_userComments($story,$A['title'],'article',$order,$mode));
+            // if (DB_count($_TABLES['pollquestions'],'qid',$story) > 0) {
+            //     $display .= COM_showPoll(80,$story);
+            // }
+
+            $story_template->set_var('formatted_article', COM_article($A, 'n'));
+            // Display the comments, if there are any ..
+            if ($A['commentcode'] >= 0) {
+                $story_template->set_var ('commentbar',
+                        COM_userComments ($story, $A['title'], 'article',
+                                          $order, $mode));
+            }
+            $display .= $story_template->finish ($story_template->parse ('output', 'article'));
+            $display .= COM_siteFooter ();
         }
-        $story_template->parse('output', 'article');
-        $display .= $story_template->finish($story_template->get_var('output'));
-        $display .= COM_siteFooter();
     }
 } else {
     $display .= COM_refresh($_CONF['site_url'] . '/index.php');
