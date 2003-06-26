7ba7492ea955ee133f45b5c45fbf96b4530f28ce
dhaun <devnull@localhost>
2003-06-26 18:36:44 +0000

    Display all static pages that have the center block flag set, not only the first one. Also added a sort option (sort by id, date, title) for those.

--- plugins/staticpages/config.php
+++ plugins/staticpages/config.php
@@ -2,6 +2,12 @@
 
 $_SP_CONF['version'] = '1.3';
 
+// If you have more than one static page that is to be displayed in Geeklog's 
+// center area, you can specify how to sort them:
+
+$_SP_CONF['sort_by'] = 'id'; // can be 'id', 'title', 'date'
+
+
 // If you experience timeout issues, you may need to set both of the
 // following values to 0 as they are intensive
 
