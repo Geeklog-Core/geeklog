8b624b407b1e68c560de3c205731011587b3e2e8
dhaun <devnull@localhost>
2004-02-14 17:20:41 +0000

    When a user is deleted, static pages created by that user can now either be deleted or assigned to a Root user.

--- plugins/staticpages/config.php
+++ plugins/staticpages/config.php
@@ -14,14 +14,22 @@ $_SP_CONF['allow_php'] = 1;
 $_SP_CONF['sort_by'] = 'id'; // can be 'id', 'title', 'date'
 
 
+// When a user is deleted, ownership of static pages created by that user can
+// be transfered to a user in the Root group (= 0) or the pages can be
+// deleted (= 1).
+$_SP_CONF['delete_pages'] = 0;
+
+
+// Static pages can optionally be wrapped in a block. This setting defines
+// the default for that option (1 = wrap in a block, 0 = don't).
+$_SP_CONF['in_block'] = 1;
+
+
 // If you experience timeout issues, you may need to set both of the
 // following values to 0 as they are intensive
 
 // NOTE: using filter_html will render any blank pages useless
 $_SP_CONF['filter_html'] = 0;
 $_SP_CONF['censor'] = 1;
-
-// set to 1 if static pages should be wrapped in a block
-$_SP_CONF['in_block'] = 1;
 
 ?>
