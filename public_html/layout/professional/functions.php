9f741eda81fc6bde20178f86ee3e8119ec726535
dhaun <devnull@localhost>
2004-09-26 10:00:00 +0000

    Initial import of the Professional theme

---
+++ public_html/layout/professional/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+$result = DB_query ("SELECT onleft,name FROM {$_TABLES['blocks']} WHERE is_enabled = 1");
+$nrows = DB_numRows ($result);
+for ($i = 0; $i < $nrows; $i++) {
+    $A = DB_fetchArray ($result);
+        if ($A['onleft'] == 1) {
+            $_BLOCK_TEMPLATE[$A['name']] = 'blockheader-left.thtml,blockfooter-left.thtml';
+        } else {
+            $_BLOCK_TEMPLATE[$A['name']] = 'blockheader-right.thtml,blockfooter-right.thtml';
+    }
+}
+
+$_BLOCK_TEMPLATE['customlogin'] = 'customlogin-header.thtml,customlogin-footer.thtml';
+$_BLOCK_TEMPLATE['whats_related_block'] = 'blockheader-related.thtml,blockfooter-related.thtml';
+$_BLOCK_TEMPLATE['story_options_block'] = 'blockheader-related.thtml,blockfooter-related.thtml';
+
+?>
