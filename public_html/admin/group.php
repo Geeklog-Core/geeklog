d2f181820ca764c0674272c7ce841264342f4c30
dhaun <devnull@localhost>
2004-10-19 10:52:48 +0000

    Don't rely on PHP_SELF

--- public_html/admin/group.php
+++ public_html/admin/group.php
@@ -32,7 +32,7 @@
 // |                                                                           |
 // +---------------------------------------------------------------------------+
 //
-// $Id: group.php,v 1.41 2004/09/29 10:36:29 dhaun Exp $
+// $Id: group.php,v 1.42 2004/10/19 10:52:48 dhaun Exp $
 
 /**
 * This file is the Geeklog Group administration page
@@ -696,7 +696,7 @@ function grp_selectUsers ($group_id = '0', $allusers = false)
 
 function editusers ($group)
 {
-    global $_CONF, $_TABLES, $_USER, $LANG_ACCESS, $HTTP_SERVER_VARS;
+    global $_CONF, $_TABLES, $_USER, $LANG_ACCESS;
 
     $thisUsersGroups = SEC_getUserGroups ();
     if (!empty ($group) && ($group > 0) &&
@@ -719,8 +719,9 @@ function editusers ($group)
                        COM_getBlockTemplate ('_admin_block', 'header'));
     $groupmembers = new Template($_CONF['path_layout'] . 'admin/group');
     $groupmembers->set_file (array ('groupmembers'=>'groupmembers.thtml'));
-    $groupmembers->set_var ('site_url',$_CONF['site_url']);
-    $groupmembers->set_var ('phpself', $HTTP_SERVER_VARS['PHP_SELF']);
+    $groupmembers->set_var ('site_url', $_CONF['site_url']);
+    $groupmembers->set_var ('site_admin_url', $_CONF['site_admin_url']);
+    $groupmembers->set_var ('phpself', $_CONF['site_admin_url'] . '/group.php');
     $groupmembers->set_var ('LANG_sitemembers',$LANG_ACCESS['availmembers']);
     $groupmembers->set_var ('LANG_grpmembers',$LANG_ACCESS['groupmembers']);
     $groupmembers->set_var ('sitemembers', grp_selectUsers($group,true) );
