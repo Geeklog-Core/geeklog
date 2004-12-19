629cb2b4cee92c6211069494448298050c780507
dhaun <devnull@localhost>
2004-12-19 14:20:23 +0000

    Set version number to 1.3.11rc1

--- config.php
+++ config.php
@@ -33,7 +33,7 @@
 // | on configuration.                                                         |
 // +---------------------------------------------------------------------------+
 //
-// $Id: config.php,v 1.146 2004/11/28 09:54:06 dhaun Exp $
+// $Id: config.php,v 1.147 2004/12/19 14:20:23 dhaun Exp $
 
 // When setting up Geeklog for the first time, you need to make sure the
 // settings in the following 3 sections are correct:
@@ -698,7 +698,7 @@ if (!defined ('LB')) {
     define('LB',"\n");
 }
 if (!defined ('VERSION')) {
-    define('VERSION', '1.3.10');
+    define('VERSION', '1.3.11rc1');
 }
 
 $_STATES = array(
