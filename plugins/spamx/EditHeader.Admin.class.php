495d5f39aaab3c6bf4c1b93af4d75fa28641aa2b
dhaun <devnull@localhost>
2005-04-02 13:29:03 +0000

    New SpamX module to filter by the HTTP request headers (to do: move text strings to the language files ...)

---
+++ plugins/spamx/EditHeader.Admin.class.php
@@ -0,0 +1,89 @@
+<?php
+/**
+* File: EditHeader.Admin.class.php
+* This is the Edit HTTP Header Module for the Geeklog SpamX Plug-in!
+*
+* Copyright (C) 2005 by the following authors:
+* Author    Dirk Haun <dirk AT haun-online DOT de>
+*
+* based on the works of Tom Willett <tomw AT pigstye DOT net>
+*
+* Licensed under GNU General Public License
+*
+* $Id: EditHeader.Admin.class.php,v 1.1 2005/04/02 13:29:03 dhaun Exp $
+*/
+
+/**
+* HTTP Header Editor
+*/
+
+require_once($_CONF['path'] . 'plugins/spamx/BaseAdmin.class.php');
+
+class EditHeader extends BaseAdmin {
+    /**
+     * Constructor
+     */
+    function display()
+    {
+        global $_CONF, $_GET, $_POST, $_TABLES, $LANG_SX00;
+
+        $action = COM_applyFilter ($_GET['action']);
+        if (empty ($action)) {
+            $action = COM_applyFilter ($_POST['paction']);
+        }
+
+        if ($action == 'delete') {
+            $entry = $_GET['entry'];
+            if (!empty ($entry)) {
+                $dbentry = addslashes ($entry);
+                $result = DB_query ("DELETE FROM {$_TABLES['spamx']} WHERE name='HTTPHeader' AND value='$dbentry'");
+            }
+        } elseif ($action == $LANG_SX00['addentry']) {
+            $entry = '';
+            $name = COM_applyFilter ($_REQUEST['header-name']);
+            $n = explode (':', $name);
+            $name = $n[0];
+            $value = $_REQUEST['header-value'];
+
+            if (!empty ($name) && !empty ($value)) {
+                $entry = $name . ': ' . $value;
+            }
+
+            $dbentry = addslashes ($entry);
+            if (!empty ($entry)) {
+                $result = DB_query ("INSERT INTO {$_TABLES['spamx']} VALUES ('HTTPHeader','$dbentry')");
+            }
+        }
+
+        $display = '<hr><p><b>';
+        $display .= $LANG_SX00['pblack'];
+        $display .= '</b></p><ul>';
+        $result = DB_query ("SELECT value FROM {$_TABLES['spamx']} WHERE name='HTTPHeader' ORDER BY value");
+        $nrows = DB_numRows ($result);
+        for ($i = 0; $i < $nrows; $i++) {
+            list($e) = DB_fetchArray ($result);
+
+            $display .= '<li><a href="' . $_CONF['site_admin_url'] . '/plugins/spamx/index.php?command=EditHeader&amp;action=delete&amp;entry=' . urlencode ($e) . '">' . $e . '</a></li>';
+        }
+        $display .= '</ul><p>' . $LANG_SX00['e1'] . '</p>';
+        $display .= '<p>' . $LANG_SX00['e2'] . '</p>';
+
+        $display .= '<form method="POST" action="' . $_CONF['site_admin_url'] . '/plugins/spamx/index.php?command=EditHeader">';
+        $display .= '<table border="0" width="100%">' . LB;
+        $display .= '<tr><td align="right"><b>Header:</b></td>' . LB;
+        $display .= '<td><input type="text" site="40" name="header-name"> e.g. <tt>User-Agent</tt></td></tr>' . LB;
+        $display .= '<tr><td align="right"><b>Content:</b></td>' . LB;
+        $display .= '<td><input type="text" site="40" name="header-value"> e.g. <tt>Mozilla</tt></td></tr>' . LB;
+        $display .= '</table>' . LB;
+        $display .= '<p><input type="Submit" name="paction" value="' . $LANG_SX00['addentry'] . '">';
+        $display .= '</form>';
+        return $display;
+    }
+
+    function link()
+    {
+        return "Edit HTTP Header Blacklist";
+    }
+}
+
+?>
