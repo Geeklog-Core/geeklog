655106d58073665b6d50109e83b3f19a7ee57d1b
blaine <devnull@localhost>
2005-06-03 04:23:30 +0000

    Defined 4 custom toolbar settings  "editor-toolbar1" thru "editor-toolbar4" With increasing functionality and toolbar icons
    Also setup a very basic defintion 'basic' that can be used for the non-editor story submit and comment forms.
    
    Will be adding options to allow user to select toolbar preference.
    
    For now - you can change the toolbar in the storyeditor_advanced.thtml file
    Top of template, example:   oFCKeditor1.ToolbarSet = 'editor-toolbar3' ;

--- public_html/fckeditor/fckconfig.js
+++ public_html/fckeditor/fckconfig.js
@@ -55,13 +55,46 @@ FCKConfig.GeckoUseSPAN    = true ;
 FCKConfig.StartupFocus    = true ;
 FCKConfig.ForcePasteAsPlainText    = false ;
 FCKConfig.ForceSimpleAmpersand    = false ;
-FCKConfig.TabSpaces        = 8 ;
+FCKConfig.TabSpaces        = 0 ;
 FCKConfig.ShowBorders    = true ;
 FCKConfig.UseBROnCarriageReturn    = false ;
 FCKConfig.ToolbarStartExpanded    = false ;
 FCKConfig.ToolbarCanCollapse    = true ;
 
 FCKConfig.ContextMenu = ['Generic','Link','Anchor','Image','Select','Textarea','Checkbox','Radio','TextField','HiddenField','ImageButton','Button','BulletedList','NumberedList','TableCell','Table','Form'] ;
+
+
+FCKConfig.ToolbarSets["editor-toolbar1"] = [
+    ['Source','-','Undo','Redo','-','Link','Unlink','-','Bold','Italic',
+	'-','JustifyLeft','JustifyCenter','JustifyRight',
+	'-','OrderedList','UnorderedList','Outdent','Indent','About']
+] ;
+
+
+FCKConfig.ToolbarSets["editor-toolbar2"] = [
+    ['Source','-','Undo','Redo','-','Link','Unlink','-','Bold','Italic','Underline','StrikeThrough',
+	'-','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull',
+	'-','OrderedList','UnorderedList','Outdent','Indent'],
+	['PasteText','PasteWord','-','FontName','FontSize','TextColor','BGColor','-','Rule','-','Image','Table','-','About']
+] ;
+
+FCKConfig.ToolbarSets["editor-toolbar3"] = [
+    ['Source','Templates','-','Cut','Copy','Paste','PasteText','PasteWord','-',
+	'Undo','Redo','-','Link','Unlink','-','SpellCheck','Rule','-',
+	'Bold','Italic','Underline','StrikeThrough','-','Image','Table'],
+	['TextColor','BGColor','-','JustifyLeft','JustifyCenter','JustifyRight','-',
+	'OrderedList','UnorderedList','-','Outdent','Indent','FontName','FontSize','-','About']
+] ;
+
+FCKConfig.ToolbarSets["editor-toolbar4"] = [
+    ['Source','Templates','-','Cut','Copy','Paste','PasteText','PasteWord','-',
+	'Find','Replace','-','Undo','Redo','-','RemoveFormat','-','Link','Unlink','-',
+	'Image','SpecialChar','-','Print','SpellCheck'],
+    ['Table','Rule','Bold','Italic','Underline','StrikeThrough','-',
+	'Subscript','Superscript','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','-',
+	'OrderedList','UnorderedList','-','Outdent','Indent','-','TextColor','BGColor','-','About'],
+    ['Style','-','FontFormat','-','FontName','-','FontSize']
+] ;
 
 FCKConfig.ToolbarSets["Default"] = [
     ['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
@@ -78,24 +111,11 @@ FCKConfig.ToolbarSets["Default"] = [
     ['About']
 ] ;
 
-FCKConfig.ToolbarSets["editor"] = [
-    ['Source','Templates','-','Cut','Copy','Paste','PasteText','PasteWord','-'],
-	['Find','Replace','-','Undo','Redo','-','RemoveFormat','-','Link','Unlink','-'],
-	['Image','SpecialChar'] ,'-',['Print','SpellCheck'],
-    ['Table','Rule','Bold','Italic','Underline','StrikeThrough','-'],
-	['Subscript','Superscript','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','-'],
-	['OrderedList','UnorderedList','-','Outdent','Indent','-','TextColor','BGColor'] ,['About'],
-    ['Style','-','FontFormat','-','FontName','-','FontSize']
-] ;
 
-FCKConfig.ToolbarSets["Basic"] = [
-    ['Bold','Italic','-','OrderedList','UnorderedList','-','Link','Unlink','-','About']
+FCKConfig.ToolbarSets["basic"] = [
+    ['Bold','Italic','Underline','StrikeThrough','-','JustifyLeft','JustifyCenter','JustifyRight',
+	'-','OrderedList','UnorderedList','-','Link','Unlink','-','Undo','Redo','-','About']
 ] ;
-
-FCKConfig.ToolbarSets["Minimal"] = [
-    ['Source','-','Bold','Italic','Underline','-','JustifyLeft','JustifyCenter','JustifyRight','-','OrderedList','UnorderedList','-','Outdent','Indent','Rule']
-] ;
-
 
 FCKConfig.FontColors = '000000,993300,333300,003300,003366,000080,333399,333333,800000,FF6600,808000,808080,008080,0000FF,666699,808080,FF0000,FF9900,99CC00,339966,33CCCC,3366FF,800080,999999,FF00FF,FFCC00,FFFF00,00FF00,00FFFF,00CCFF,993366,C0C0C0,FF99CC,FFCC99,FFFF99,CCFFCC,CCFFFF,99CCFF,CC99FF,FFFFFF' ;
 
