8e639dbed07474d93cb7d3d83f0c85702418fdf7
dhaun <devnull@localhost>
2005-09-25 09:10:39 +0000

    Updated text, now that there is only one default account on new installs.

--- public_html/admin/install/success.php
+++ public_html/admin/install/success.php
@@ -6,14 +6,15 @@ $display = COM_siteHeader ('menu', 'Installation complete');
 $display .= COM_startBlock ('Installation complete');
 $display .= '<h2>Installation of Geeklog ' . VERSION . ' complete!</h2>';
 
-$display .= '<p>Congratulations, you have successfully installed Geeklog. Please take a minute to read the information displayed below. Then see the first story on your new site to learn about the default login.</p>';
-$display .= '<p><a href="' . $_CONF['site_url'] . '">Click here</a> to go to your site\'s front page.</p>';
+$display .= '<p>Congratulations, you have successfully installed Geeklog. Please take a minute to read the information displayed below.</p>';
+
+$display .= '<p>Then <a href="' . $_CONF['site_url'] . '">click here</a> to go to your site\'s front page and <b>learn about the default login</b>.</p>';
 
 $display .= '<h2>Check Permissions</h2>';
 $display .= '<p>Geeklog requires certain files and directories to be writable. To check if those are set up properly, please use <a href="check.php">this script</a>.</p>';
 
 $display .= '<h2>Security Warning</h2>';
-$display .= '<p>Once your site is up and running, don\'t forget to <strong>remove the install directory</strong>, <tt>' . $_CONF['path_html'] . 'admin/install</tt>, and <strong>change the passwords</strong> of <em>both</em> default accounts.</p>';
+$display .= '<p>Once your site is up and running, don\'t forget to <strong>remove the install directory</strong>, <tt>' . $_CONF['path_html'] . 'admin/install</tt>, and <strong>change the password</strong> of the default \'Admin\' account.</p>';
 
 // note for those upgrading from Geeklog 1.2.5-1 or older
 if (DB_count ($_TABLES['users'], 'username', 'NewAdmin') > 0) {
