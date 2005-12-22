59f65c93b2c9826ca426f9495ab387532b3fc86a
dhaun <devnull@localhost>
2005-12-22 11:56:38 +0000

    Updated Ukrainian (Windows-1251) language file and new matching plugin language files, provided by Vitaliy Biliyenko

---
+++ plugins/links/language/ukrainian.php
@@ -0,0 +1,115 @@
+<?php
+
+###############################################################################
+# ukrainian.php
+# This is the ukrainian language page for the Geeklog Links Plug-in!
+#
+# Copyright (C) 2005 Vitaliy Biliyenko
+# v.lokki@gmail.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+###############################################################################
+# $Id: ukrainian.php,v 1.1 2005/12/22 11:56:37 dhaun Exp $
+
+###############################################################################
+# Array Format:
+# $LANGXX[YY]:	$LANG - variable name
+#		  	XX - file id number
+#			YY - phrase id number
+###############################################################################
+
+
+$LANG_LINKS= array(
+    10 => 'Надіслане',
+    14 => 'Посилання',
+    84 => 'Посилання',
+    88 => 'Немає свіжих посилань',
+    114 => 'Веб ресурси',
+    116 => 'Додати посилання'
+);
+
+###############################################################################
+# for stats
+
+$LANG_LINKS_STATS = array(
+    'links' => 'Лінків (Кліків) у системі',
+    'stats_headline' => '10 найпопулярніших лінків',
+    'stats_page_title' => 'Посилання',
+    'stats_hits' => 'Хіти',
+    'stats_no_hits' => 'На цьому сайті немає посилань, або ще ніхто ними не користувався.',
+);
+
+###############################################################################
+# for the search
+
+$LANG_LINKS_SEARCH = array(
+ 'results' => 'Результати з посилань',
+ 'title' => 'Заголовок',
+ 'date' => 'Додано',
+ 'author' => 'Автор',
+ 'hits' => 'Кліків'
+);
+###############################################################################
+# for the submission form
+
+$LANG_LINKS_SUBMIT = array(
+    1 => 'Надіслати посилання',
+    2 => 'Посилання',
+    3 => 'Категорія',
+    4 => 'Інше',
+    5 => 'Якщо інше, зазначте',
+    6 => 'Помилка: Відсутня категорія',
+    7 => 'Обираючи "Інше", будь-ласка, вкажіть назву категорії',
+    8 => 'Заголовок',
+    9 => 'URL',
+    10 => 'Категорія',
+    11 => 'Надіслані посилання'
+);
+
+###############################################################################
+# Messages for COM_showMessage the submission form
+
+$PLG_links_MESSAGE1 = "Дякуємо за надіслане на {$_CONF['site_name']} посилання.  Його передано нашому персоналу для схвалення.  У разі схвалення його буде додано до розділу <a href={$_CONF['site_url']}/links/index.php>Веб ресурси</a>.";
+$PLG_links_MESSAGE2 = 'Ваше посилання успішно збережено.';
+$PLG_links_MESSAGE3 = 'Посилання успішно вилучено.';
+$PLG_links_MESSAGE4 = "Дякуємо за надіслане на {$_CONF['site_name']} посилання.  Його додано до розділу <a href={$_CONF['site_url']}/links/index.php>Веб ресурси</a>.";
+
+###############################################################################
+# admin/link.php
+
+$LANG_LINKS_ADMIN = array(
+    1 => 'Редактор посилань',
+    2 => 'ID посилання',
+    3 => 'Заголовок',
+    4 => 'URL',
+    5 => 'Категорія',
+    6 => '(включаючи http://)',
+    7 => 'Інше',
+    8 => 'Хіти посилання',
+    9 => 'Опис',
+    10 => 'Ви повинні вказати заголовок, URL та опис.',
+    11 => 'Менеджер посилань',
+    12 => 'Щоб змінити чи видалити посилання, натисніть його іконку редагування нижче.  Щоб створити нове посилання, оберіть "Створити нове" вгорі.',
+    14 => 'Категорія посилання',
+    16 => 'Доступ заборонено',
+    17 => "Ви намагались отримати доступ до посилання, до якого у вас немає прав.  Цю спробу записано. Будь-ласка, <a href=\"{$_CONF['site_admin_url']}/plugins/links/index.php\">поверніться до адміністрування</a>.",
+    20 => 'Якщо інше, зазначте',
+    21 => 'зберегти',
+    22 => 'скасувати',
+    23 => 'вилучити'
+);
+
+?>
---
+++ plugins/staticpages/language/ukrainian.php
@@ -0,0 +1,114 @@
+<?php
+
+###############################################################################
+# ukrainian.php
+# This is the ukrainian language page for the Geeklog Static Page Plug-in!
+#
+# Copyright (C) 2005 Vitaliy Biliyenko
+# v.lokki@gmail.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+###############################################################################
+
+###############################################################################
+# Array Format: 
+# $LANGXX[YY]:	$LANG - variable name
+#		  	XX - file id number
+#			YY - phrase id number
+###############################################################################
+
+
+$LANG_STATIC= array(
+    'newpage' => 'Нова сторінка',
+    'adminhome' => 'Home адміністратора',
+    'staticpages' => 'Статичні сторінки',
+    'staticpageeditor' => 'Редактор статичних сторінок',
+    'writtenby' => 'Автор',
+    'date' => 'Востаннє оновлено',
+    'title' => 'Заголовок',
+    'content' => 'Зміст',
+    'hits' => 'Хіти',
+    'staticpagelist' => 'Список статичних сторінок',
+    'url' => 'URL',
+    'edit' => 'Редагувати',
+    'lastupdated' => 'Востаннє оновлено',
+    'pageformat' => 'Формат сторінки',
+    'leftrightblocks' => 'Лівий та правий блоки',
+    'blankpage' => 'Порожня сторінка',
+    'noblocks' => 'Без блоків',
+    'leftblocks' => 'Ліві блоки',
+    'addtomenu' => 'Додати до меню',
+    'label' => 'Мітка',
+    'nopages' => 'В системі ще немає статичних сторінок',
+    'save' => 'зберегти',
+    'preview' => 'перегляд',
+    'delete' => 'видалити',
+    'cancel' => 'відмінити',
+    'access_denied' => 'Доступ заборонено',
+    'access_denied_msg' => 'Ви намагаєтесь нелегально отримати доступ до одного з розділів адміністрування статичних сторінок.  Будь ласка, зважте, що всі спроби нелегального доступу до цієї сторінки вносять до протоколу',
+    'all_html_allowed' => 'Весь HTML дозволено',
+    'results' => 'Результати серед статичних сторінок',
+    'author' => 'Автор',
+    'no_title_or_content' => 'Вам необхідно принаймні заповнити поля <b>Заголовок</b> та <b>Зміст</b>.',
+    'no_such_page_logged_in' => 'Вибачте '.$_USER['username'].'..',
+    'no_such_page_anon' => 'Будь ласка, увійдіть ..',
+    'no_page_access_msg' => "Можливо, це трапилось тому, що ви не увійшли, або не є членом {$_CONF['site_name']}. Будь ласка, <a href=\"{$_CONF['site_url']}/users.php?mode=new\"> зареєструйтесь </a>на {$_CONF['site_name']} для отримання повного доступу",
+    'php_msg' => 'PHP: ',
+    'php_warn' => 'Попередження: PHP код вашої сторінки буде виконано якщо ви активуєте цю опцію. Будьте обережні!!',
+    'exit_msg' => 'Тип виходу: ',
+    'exit_info' => 'Скористайтесь повідомленням для входу.  Залиште невідміченим для звичайного контролю безпеки та повідомлень.',
+    'deny_msg' => 'Доступ до цієї сторінки заборонено. Сторінка або була переміщена/видалена, або у вас немає відповідного доступу.',
+    'stats_headline' => '10 найпопулярніших статичних сторінок',
+    'stats_page_title' => 'Заголовок сторінки',
+    'stats_hits' => 'Хіти',
+    'stats_no_hits' => 'На цьому сайті немає статичних сторінок або ніхто їх не відвідував.',
+    'id' => 'ID',
+    'duplicate_id' => 'Обраний вами ID для цієї сторінки вже використовується. Будь ласка, виберіть інший ID.',
+    'instructions' => 'Щоб змінити чи видалити статичну сторінку, натисніть на її іконку редагування нижче. Щоб переглянути сторінку, натисніть на її заголовку. Щоб створити нову статичну сторінку, оберіть Створити нове вгорі. Натисніть іконку копії, щоб скопіювати існуючу сторінку.',
+    'centerblock' => 'Центральний блок: ',
+    'centerblock_msg' => 'Якщо ви відмітите, ця статична сторінка буде виводитись як центральний блок на головній сторінці.',
+    'topic' => 'Тема: ',
+    'position' => 'Розміщення: ',
+    'all_topics' => 'Всі',
+    'no_topic' => 'Лише домашня сторінка',
+    'position_top' => 'Вгорі сторінки',
+    'position_feat' => 'Після Особливої статті',
+    'position_bottom' => 'Внизу сторінки',
+    'position_entire' => 'На всю сторінку',
+    'head_centerblock' => 'Центральний блок',
+    'centerblock_no' => 'Ні',
+    'centerblock_top' => 'Верх',
+    'centerblock_feat' => 'Особ. стаття',
+    'centerblock_bottom' => 'Низ',
+    'centerblock_entire' => 'Вся сторінка',
+    'inblock_msg' => 'У блоці: ',
+    'inblock_info' => 'Помістити статичну сторінку в блок.',
+    'title_edit' => 'Редагувати сторінку',
+    'title_copy' => 'Копіювати сторінку',
+    'title_display' => 'Переглянути сторінку',
+    'select_php_none' => 'не виконувати PHP',
+    'select_php_return' => 'виконувати PHP (return)',
+    'select_php_free' => 'виконувати PHP',
+    'php_not_activated' => 'Використання PHP у статичних сторінках не активоване. Будь ласка, подивіться в <a href="' . $_CONF['site_url'] . '/docs/staticpages.html#php">документацію</a> щодо деталей.',
+    'printable_format' => 'Формат для друку',
+	'edit' => 'Редагувати',
+    'copy' => 'Копіювати',
+    'limit_results' => 'Обмежити результати',
+    'search' => 'Пошук',
+    'submit' => 'Відіслати'
+);
+
+?>
