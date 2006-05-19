3ad16fbc95a48381209988e2d7e96b158630aa67
ospiess <devnull@localhost>
2006-05-19 06:10:18 +0000

    creating new month & week strings

--- language/english.php
+++ language/english.php
@@ -1325,6 +1325,30 @@ $LANG_WHATSNEW = array (
     'month'       => 'month'
 );
 
+$LANG_MONTH = array(
+    1 => 'January',
+    2 => 'February',
+    3 => 'March',
+    4 => 'April',
+    5 => 'May',
+    6 => 'June',
+    7 => 'July',
+    8 => 'August',
+    9 => 'September',
+    10 => 'October',
+    11 => 'November',
+    12 => 'December'
+);
+
+$LANG_WEEK = array(
+    1 => 'Sunday',
+    2 => 'Monday',
+    3 => 'Tuesday',
+    4 => 'Wednesday',
+    5 => 'Thurdsay',
+    6 => 'Friday',
+    7 => 'Saturday'
+);
 ################################################################################
 # Admin - Strings
 #
