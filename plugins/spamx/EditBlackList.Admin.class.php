494ce70aee5a991fc84be124756b573f8a6ce691
dhaun <devnull@localhost>
2006-06-18 18:32:01 +0000

    Don't filter blacklist entries as this would strip special characters needed for the regular expressions; some source code cosmetics

--- plugins/spamx/EditBlackList.Admin.class.php
+++ plugins/spamx/EditBlackList.Admin.class.php
@@ -2,21 +2,26 @@
 
 /**
  * File: EditBlackList.Admin.class.php
- * This is the Edit Personal Blacklist Module for the Geeklog Spam-X Plug-in!
- * 
- * Copyright (C) 2004-2005 by the following authors:
- * Author		Tom Willett		tomw AT pigstye DOT net
- * 
+ * This is the Edit Personal Blacklist Module for the Geeklog Spam-X plugin
+ *
+ * Copyright (C) 2004-2006 by the following authors:
+ * Author   Tom Willett     tomw AT pigstye DOT net
+ *          Dirk Haun       dirk AT haun-online DOT de
+ *
  * Licensed under GNU General Public License
  *
- * $Id: EditBlackList.Admin.class.php,v 1.4 2005/04/10 10:02:44 dhaun Exp $
+ * $Id: EditBlackList.Admin.class.php,v 1.5 2006/06/18 18:32:01 dhaun Exp $
  */
+
+if (strpos ($_SERVER['PHP_SELF'], 'EditBlackList.Admin.class.php') !== false) {
+    die ('This file can not be used on its own!');
+}
 
 /**
  * Personal Black List Editor
  */
 
-require_once($_CONF['path'] . 'plugins/spamx/BaseAdmin.class.php');
+require_once ($_CONF['path'] . 'plugins/spamx/BaseAdmin.class.php');
 
 class EditBlackList extends BaseAdmin {
     /**
@@ -24,57 +29,63 @@ class EditBlackList extends BaseAdmin {
      */
     function display()
     {
-        global $_CONF, $_GET, $_POST, $_TABLES, $LANG_SX00;
+        global $_CONF, $_TABLES, $LANG_SX00;
 
         require_once $_CONF['path'] . 'plugins/spamx/rss.inc.php';
 
-        $action = COM_applyFilter($_GET['action']);
-        if (empty($action)) {
-            $action = COM_applyFilter($_POST['paction']);
+        $action = COM_applyFilter ($_GET['action']);
+        if (empty ($action)) {
+            $action = COM_applyFilter ($_POST['paction']);
         } 
 
-        $entry = COM_applyFilter($_GET['entry']);
-        if (empty($entry)) {
-            $entry = COM_applyFilter($_POST['pentry']);
-        } 
+        $entry = '';
+        if (isset ($_GET['entry'])) {
+            $entry = COM_stripslashes ($_GET['entry']);
+        } else if (isset ($_POST['pentry'])) {
+            $entry = COM_stripslashes ($_POST['pentry']);
+        }
 
         if ($action == 'delete') {
-            $result = DB_query('DELETE FROM ' . $_TABLES['spamx'] . ' where name="Personal" AND value="' . $entry . '"');
+            $entry = addslashes ($entry);
+            $result = DB_query ("DELETE FROM {$_TABLES['spamx']} WHERE name = 'Personal' AND value = '$entry'");
         } elseif ($action == $LANG_SX00['addentry']) {
-            if ($entry != "") {
-                $result = DB_query('INSERT INTO ' . $_TABLES['spamx'] . ' VALUES ("Personal","' . $entry . '")');
-            } 
+            if (!empty ($entry)) {
+                $entry = addslashes ($entry);
+                $result = DB_query ("INSERT INTO {$_TABLES['spamx']} VALUES ('Personal', '$entry')");
+            }
         } elseif ($action == $LANG_SX00['addcen']) {
-            foreach($_CONF['censorlist'] as $entry) {
-                $result = DB_query('INSERT INTO ' . $_TABLES['spamx'] . ' VALUES ("Personal","' . $entry . '")');
-            } 
-        } 
+            foreach ($_CONF['censorlist'] as $entry) {
+                $entry = addslashes ($entry);
+                $result = DB_query ("INSERT INTO {$_TABLES['spamx']} VALUES ('Personal', '$entry')");
+            }
+        }
 
-        $display = '<hr><p><b>';
+        $display = '<hr>' . LB . '<p><b>';
         $display .= $LANG_SX00['pblack'];
-        $display .= '</b></p><ul>';
-        $result = DB_query('SELECT * FROM ' . $_TABLES['spamx'] . ' WHERE name="Personal"');
-        $nrows = DB_numRows($result);
-        for($i = 1;$i <= $nrows;$i++) {
-            $A = DB_fetchArray($result);
+        $display .= '</b></p>' . LB . '<ul>' . LB;
+        $result = DB_query ("SELECT value FROM {$_TABLES['spamx']} WHERE name = 'Personal'");
+        $nrows = DB_numRows ($result);
+        for ($i = 0; $i < $nrows; $i++) {
+            $A = DB_fetchArray ($result);
             $e = $A['value'];
-            $display .= '<li><a href="' . $_CONF['site_admin_url'] . '/plugins/spamx/index.php?command=EditBlackList&action=delete&entry=' . urlencode($e) . '">' . $e . '</a></li>';
+            $display .= '<li><a href="' . $_CONF['site_admin_url'] . '/plugins/spamx/index.php?command=EditBlackList&amp;action=delete&amp;entry=' . urlencode ($e) . '">' . htmlspecialchars ($e) . '</a></li>' . LB;
         } 
-        $display .= '</ul><p>' . $LANG_SX00['e1'] . '</p>';
-        $display .= '<p>' . $LANG_SX00['e2'] . '</p>';
-        $display .= '<form method="post" action="' . $_CONF['site_admin_url'] . '/plugins/spamx/index.php?command=EditBlackList">';
-        $display .= '<input type="text" size ="30" name="pentry">&nbsp;&nbsp;&nbsp;';
-        $display .= '<input type="Submit" name="paction" value="' . $LANG_SX00['addentry'] . '">';
+        $display .= '</ul>' . LB . '<p>' . $LANG_SX00['e1'] . '</p>' . LB;
+        $display .= '<p>' . $LANG_SX00['e2'] . '</p>' . LB;
+        $display .= '<form method="POST" action="' . $_CONF['site_admin_url'] . '/plugins/spamx/index.php?command=EditBlackList">' . LB;
+        $display .= '<input type="text" size="30" name="pentry">&nbsp;&nbsp;&nbsp;';
+        $display .= '<input type="submit" name="paction" value="' . $LANG_SX00['addentry'] . '">' . LB;
         $display .= '<p>' . $LANG_SX00['e3'] . '&nbsp&nbsp&nbsp';
-        $display .= '<input type = "Submit" name="paction" value="' . $LANG_SX00['addcen'] . '">';
-        $display .= '</form>';
+        $display .= '<input type="submit" name="paction" value="' . $LANG_SX00['addcen'] . '">' . LB;
+        $display .= '</form>' . LB;
+
         return $display;
-    } 
+    }
 
     function link()
     {
-        return "Edit Personal Blacklist";
-    } 
-} 
+        return 'Edit Personal Blacklist';
+    }
+}
 
 ?>
