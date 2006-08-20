8eac1de2024423ec08809ad1cdc42ecb55d28897
blaine <devnull@localhost>
2006-08-20 03:38:38 +0000

    Added support for a common javascript library so re-useable JS functions can be added without having to modify the header.thtml

---
+++ public_html/javascript/common.js
@@ -0,0 +1,47 @@
+/* Reminder: always indent with 4 spaces (no tabs). */
+// +---------------------------------------------------------------------------+
+// | Geeklog 1.3                                                               |
+// +---------------------------------------------------------------------------+
+// | Commmon javascript functions                                              |
+// |                                                                           |
+// +---------------------------------------------------------------------------+
+// | Copyright (C) 2005,2006 by the following authors:                         |
+// |                                                                           |
+// |            Blaine Lang - blaine@portalparts.com                           |
+// +---------------------------------------------------------------------------+
+// |                                                                           |
+// | This program is free software; you can redistribute it and/or             |
+// | modify it under the terms of the GNU General Public License               |
+// | as published by the Free Software Foundation; either version 2            |
+// | of the License, or (at your option) any later version.                    |
+// |                                                                           |
+// | This program is distributed in the hope that it will be useful,           |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+// | GNU General Public License for more details.                              |
+// |                                                                           |
+// | You should have received a copy of the GNU General Public License         |
+// | along with this program; if not, write to the Free Software Foundation,   |
+// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
+// |                                                                           |
+// +---------------------------------------------------------------------------+
+
+// -------------------------------------------------------------------
+// caItems(form object)
+// Check All Items - generic function that can be used to check and un-check all items in a list
+// Used in the Admin Lists - like on the moderation page
+// -------------------------------------------------------------------
+   function caItems(f) {  
+       var n=f.elements.length;
+       for (i=0;i<n; i++) {
+           var field=f.elements[i];
+           if (field.type == 'checkbox' && field.name.match("delitem")) {
+                if (f.chk_selectall.checked) {
+                    field.checked=true;
+                } else {
+                    field.checked=false;
+                }
+           }
+
+       }
+   }
