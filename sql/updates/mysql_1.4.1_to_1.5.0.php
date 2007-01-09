552ae5ea3d8b6d799cc629f4ba506bfc36d331f8
ospiess <devnull@localhost>
2007-01-09 06:03:24 +0000

    remove old default values of user tzid since they either use the systems or a self-chosen  one

--- sql/updates/mysql_1.4.1_to_1.5.0.php
+++ sql/updates/mysql_1.4.1_to_1.5.0.php
@@ -2,6 +2,7 @@
 
 // remove time zone table since its included into PEAR now
 $_SQL[] = "DROP TABLE " . $_DB_table_prefix . 'tzcodes';
-$_SQL[] = "ALTER TABLE {$_TABLES['userprefs']} CHANGE `tzid` `tzid` VARCHAR(125) NOT NULL DEFAULT 'edt';
-
+$_SQL[] = "ALTER TABLE {$_TABLES['userprefs']} CHANGE `tzid` `tzid` VARCHAR(125) NOT NULL DEFAULT ''";
+// change former default values to '' so users dont all have edt for no reason
+$_SQL[] = "UPDATE `{$_TABLES['userprefs']}` set tzid = ''";
 ?>
