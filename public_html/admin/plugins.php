a16887cc6aee9430ea72a7c8b440b92a20fd2304
dhaun <devnull@localhost>
2007-08-04 18:31:08 +0000

    Fixed missing N/A display when no plugin version number was available (reported by machinari)

--- public_html/admin/plugins.php
+++ public_html/admin/plugins.php
@@ -32,7 +32,7 @@
 // |                                                                           |
 // +---------------------------------------------------------------------------+
 //
-// $Id: plugins.php,v 1.72 2007/03/09 03:57:08 ospiess Exp $
+// $Id: plugins.php,v 1.73 2007/08/04 18:31:08 dhaun Exp $
 
 require_once ('../lib-common.php');
 require_once ('auth.inc.php');
@@ -138,7 +138,7 @@ function plugineditor ($pi_name, $confirmed = 0)
     $plg_templates->set_var('pi_version', $A['pi_version']);
     $plg_templates->set_var('lang_geeklogversion', $LANG32[29]);
     $plg_templates->set_var('pi_gl_version', $A['pi_gl_version']);
-    $plg_templates->set_var('pi_codeversion', $plugin_code_version );
+    $plg_templates->set_var('pi_codeversion', $code_version );
     $plg_templates->set_var('lang_enabled', $LANG32[19]);
     if ($A['pi_enabled'] == 1) {
         $plg_templates->set_var('enabled_checked', 'checked="checked"');
