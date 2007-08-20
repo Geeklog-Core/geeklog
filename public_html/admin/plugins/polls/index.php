12a9eda14f085b2f0eefa4391eb2eeca9500db8b
ospiess <devnull@localhost>
2007-08-20 09:29:06 +0000

    remove debug text

--- public_html/admin/plugins/polls/index.php
+++ public_html/admin/plugins/polls/index.php
@@ -32,7 +32,7 @@
 // |                                                                           |
 // +---------------------------------------------------------------------------+
 //
-// $Id: index.php,v 1.44 2007/08/20 09:25:12 ospiess Exp $
+// $Id: index.php,v 1.45 2007/08/20 09:29:06 ospiess Exp $
 
 // Set this to true if you want to log debug messages to error.log
 $_POLL_VERBOSE = false;
@@ -213,7 +213,6 @@ function savepoll ($pid, $Q, $mainpage, $topic, $statuscode, $open, $hideresults
                     $R[$i][$j] = addslashes ($R[$i][$j]);
                     $sql = "INSERT INTO {$_TABLES['pollanswers']} (pid, qid, aid, answer, votes, remark) VALUES "
                         . "('$pid', '$k', " . ($j+1) . ", '{$A[$i][$j]}', {$V[$i][$j]}, '{$R[$i][$j]}');";
-                    echo $sql;
                     DB_query($sql);
                     $v = $v + $V[$i][$j];
                 }
