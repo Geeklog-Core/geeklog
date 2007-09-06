7421c3a248fb39961aaf8c3f5dcdf594297238f2
mwest <devnull@localhost>
2007-09-06 06:57:48 +0000

    install.php will redirect to index.php for backwards compatibility

---
+++ public_html/admin/install/install.php
@@ -0,0 +1,40 @@
+<?php
+
+/* Reminder: always indent with 4 spaces (no tabs). */
+// +---------------------------------------------------------------------------+
+// | Geeklog 1.4                                                               |
+// +---------------------------------------------------------------------------+
+// | install.php                                                               |
+// |                                                                           |
+// | Redirect to Geeklog installation script.                                  |
+// +---------------------------------------------------------------------------+
+// | Copyright (C) 2000-2007 by the following authors:                         |
+// |                                                                           |
+// | Authors: Matt West         - matt AT mattdanger DOT net                   |
+// +---------------------------------------------------------------------------+
+// |                                                                           |
+// | This program is free software; you can redistribute it and/or             |
+// | modify it under the terms of the GNU General Public License               |
+// | as published by the Free Software Foundation; either version 2            |
+// | of the License, or (at your option) any later version.                    |
+// |                                                                           |
+// | This program is distributed in the hope that it will be useful,           |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+// | GNU General Public License for more details.                              |
+// |                                                                           |
+// | You should have received a copy of the GNU General Public License         |
+// | along with this program; if not, write to the Free Software Foundation,   |
+// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
+// |                                                                           |
+// +---------------------------------------------------------------------------+
+// | You don't need to change anything in this file.                           |
+// | Please read docs/install.html which describes how to install Geeklog.     |
+// +---------------------------------------------------------------------------+
+//
+// $Id: install.php,v 1.96 2007/09/06 06:57:48 mwest Exp $
+
+// This file still exists for backwards compatibility 
+header('Location: index.php');
+
+?>
