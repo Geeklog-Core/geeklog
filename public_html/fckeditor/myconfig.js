fb43f7c28513f71e7d6916917995a78e93308ddb
blaine <devnull@localhost>
2008-02-09 12:54:01 +0000

    Upgrade to v2.5.1 of FCKeditor

---
+++ public_html/fckeditor/myconfig.js
@@ -0,0 +1,23 @@
+FCKConfig.ToolbarSets["editor-toolbar1"] = [
+    ['Source','-','Undo','Redo','-','Link','Unlink','-','Bold','Italic',
+    '-','JustifyLeft','JustifyCenter','JustifyRight',
+    '-','OrderedList','UnorderedList','Outdent','Indent','FitWindow','About']
+] ;
+
+
+FCKConfig.ToolbarSets["editor-toolbar2"] = [
+    ['Source','-','Undo','Redo','-','Link','Unlink','-','Bold','Italic','Underline','StrikeThrough',
+    '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull',
+    '-','OrderedList','UnorderedList','Outdent','Indent'],
+    ['PasteText','PasteWord','-','FontName','FontSize','TextColor','BGColor','-','Rule','-','Image','Table','FitWindow','-','About']
+] ;
+
+FCKConfig.ToolbarSets["editor-toolbar3"] = [
+    ['Source','Templates','-','Cut','Copy','Paste','PasteText','PasteWord','-',
+    'Find','Replace','-','Undo','Redo','-','RemoveFormat','-','Link','Unlink','-',
+    'Image','SpecialChar','-','Print','SpellCheck','FitWindow'],
+    ['Table','Rule','Bold','Italic','Underline','StrikeThrough','-',
+    'Subscript','Superscript','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','-',
+    'OrderedList','UnorderedList','-','Outdent','Indent','-','TextColor','BGColor','-','About'],
+    ['Style','-','FontFormat','-','FontName','-','FontSize']
+] ;
