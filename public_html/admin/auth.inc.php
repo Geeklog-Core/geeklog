95415c420fe6a0db5b29cefbbcb4d7ce16fcde8f
dhaun <devnull@localhost>
2008-03-09 16:21:18 +0000

    Check if local login is enabled before attempting to authenticate user; added missing login speedlimit

--- public_html/admin/auth.inc.php
+++ public_html/admin/auth.inc.php
@@ -31,37 +31,45 @@
 // |                                                                           |
 // +---------------------------------------------------------------------------+
 //
-// $Id: auth.inc.php,v 1.36 2008/02/17 18:32:35 dhaun Exp $
+// $Id: auth.inc.php,v 1.37 2008/03/09 16:21:18 dhaun Exp $
 
 // this file can't be used on its own
-if (strpos ($_SERVER['PHP_SELF'], 'auth.inc.php') !== false)
-{
-    die ('This file can not be used on its own.');
+if (strpos($_SERVER['PHP_SELF'], 'auth.inc.php') !== false) {
+    die('This file can not be used on its own.');
 }
 
 // MAIN
+COM_clearSpeedlimit($_CONF['login_speedlimit'], 'login');
+if (COM_checkSpeedlimit('login', $_CONF['login_attempts']) > 0) {
+    COM_displayMessageAndAbort($LANG04[112], '', 403, 'Access denied');
+}
+
 $uid = '';
-if (!empty ($_POST['loginname']) && !empty ($_POST['passwd'])) {
-    $status = SEC_authenticate (COM_applyFilter ($_POST['loginname']),
-                                $_POST['passwd'], $uid);
+if (!empty($_POST['loginname']) && !empty($_POST['passwd'])) {
+    if ($_CONF['user_login_method']['standard'])
+        $status = SEC_authenticate(COM_applyFilter($_POST['loginname']),
+                                   $_POST['passwd'], $uid);
+    } else {
+        $status = '';
+    }
 } else {
     $status = '';
 }
 $display = '';
 
 if ($status == USER_ACCOUNT_ACTIVE) {
-    DB_change($_TABLES['users'],'pwrequestid',"NULL",'uid',$uid);
-    $_USER = SESS_getUserDataFromId ($uid);
-    $sessid = SESS_newSession ($_USER['uid'], $_SERVER['REMOTE_ADDR'],
+    DB_change($_TABLES['users'], 'pwrequestid', "NULL", 'uid', $uid);
+    $_USER = SESS_getUserDataFromId($uid);
+    $sessid = SESS_newSession($_USER['uid'], $_SERVER['REMOTE_ADDR'],
             $_CONF['session_cookie_timeout'], $_CONF['cookie_ip']);
-    SESS_setSessionCookie ($sessid, $_CONF['session_cookie_timeout'],
+    SESS_setSessionCookie($sessid, $_CONF['session_cookie_timeout'],
             $_CONF['cookie_session'], $_CONF['cookie_path'],
             $_CONF['cookiedomain'], $_CONF['cookiesecure']);
-    PLG_loginUser ($_USER['uid']);
+    PLG_loginUser($_USER['uid']);
 
     // Now that we handled session cookies, handle longterm cookie
 
-    if (!isset ($_COOKIE[$_CONF['cookie_name']])) {
+    if (!isset($_COOKIE[$_CONF['cookie_name']])) {
 
         // Either their cookie expired or they are new
 
@@ -71,9 +79,9 @@ if ($status == USER_ACCOUNT_ACTIVE) {
 
             // They want their cookie to persist for some amount of time so set it now
 
-            setcookie ($_CONF['cookie_name'], $_USER['uid'],
-                       time() + $cooktime, $_CONF['cookie_path'],
-                       $_CONF['cookiedomain'], $_CONF['cookiesecure']);
+            setcookie($_CONF['cookie_name'], $_USER['uid'],
+                      time() + $cooktime, $_CONF['cookie_path'],
+                      $_CONF['cookiedomain'], $_CONF['cookiesecure']);
         }
     }
     if (!SEC_hasRights('story.edit,block.edit,topic.edit,user.edit,plugin.edit,syndication.edit','OR')) {
@@ -83,36 +91,42 @@ if ($status == USER_ACCOUNT_ACTIVE) {
     }
     echo $display;
     exit;
-} else if (!SEC_hasRights('story.edit,block.edit,topic.edit,user.edit,plugin.edit,user.mail,syndication.edit','OR') && (count (PLG_getAdminOptions()) == 0)) {
-    $display .= COM_siteHeader();
+} else if (!SEC_hasRights('story.edit,block.edit,topic.edit,user.edit,plugin.edit,user.mail,syndication.edit','OR') && (count(PLG_getAdminOptions()) == 0)) {
+    COM_updateSpeedlimit('login');
 
-    $display .= COM_startBlock($LANG20[01]);
+    $display .= COM_siteHeader('menu');
+    $display .= COM_startBlock($LANG20[1]);
 
-    if (!empty($warn)) {
-        $display .= $LANG20[02]
-        .'<br' . XHTML . '><br' . XHTML . '>'
-        .COM_accessLog($LANG20[03] . ' ' . $_POST['loginname']);
+    if (!$_CONF['user_login_method']['standard']) {
+        $display .= '<p>' . $LANG_LOGIN[2] . '</p>';
+    } else {
+
+        if (!empty($warn)) {
+            $display .= $LANG20[2]
+                     . '<br' . XHTML . '><br' . XHTML . '>'
+                     . COM_accessLog($LANG20[3] . ' ' . $_POST['loginname']);
+        }
+
+        $display .= '<form action="' . $_SERVER['PHP_SELF'] . '" method="post">'
+            .'<table cellspacing="0" cellpadding="0" border="0" width="100%">'.LB
+            .'<tr><td align="right">'.$LANG20[4].'&nbsp;</td>'.LB
+            .'<td><input type="text" name="loginname" size="16" maxlength="16"' . XHTML . '></td>'.LB
+            .'</tr>'.LB
+            .'<tr>'.LB
+            .'<td align="right">'.$LANG20[5].'&nbsp;</td>'.LB
+            .'<td><input type="password" name="passwd" size="16" maxlength="16"' . XHTML . '></td>'
+            .'</tr>'.LB
+            .'<tr>'.LB
+            .'<td colspan="2" align="center" class="warning">'.$LANG20[6].'<input type="hidden" name="warn" value="1"' . XHTML . '>'
+            .'<br' . XHTML . '><input type="submit" name="mode" value="'.$LANG20[7].'"' . XHTML . '></td>'.LB
+            .'</tr>'.LB
+            .'</table></form>';
     }
 
-    $display .= '<form action="' . $_SERVER['PHP_SELF']
-             . '" method="post">'
-        .'<table cellspacing="0" cellpadding="0" border="0" width="100%">'.LB
-        .'<tr><td align="right">'.$LANG20[04].'&nbsp;</td>'.LB
-        .'<td><input type="text" name="loginname" size="16" maxlength="16"' . XHTML . '></td>'.LB
-        .'</tr>'.LB
-        .'<tr>'.LB
-        .'<td align="right">'.$LANG20[05].'&nbsp;</td>'.LB
-        .'<td><input type="password" name="passwd" size="16" maxlength="16"' . XHTML . '></td>'
-        .'</tr>'.LB
-        .'<tr>'.LB
-        .'<td colspan="2" align="center" class="warning">'.$LANG20[06].'<input type="hidden" name="warn" value="1"' . XHTML . '>'
-        .'<br' . XHTML . '><input type="submit" name="mode" value="'.$LANG20[07].'"' . XHTML . '></td>'.LB
-        .'</tr>'.LB
-        .'</table></form>'
-        .COM_endBlock()
-        .COM_siteFooter();
-        echo $display;
-        exit;
+    $display .= COM_endBlock()
+             . COM_siteFooter();
+    echo $display;
+    exit;
 }
 
 ?>
