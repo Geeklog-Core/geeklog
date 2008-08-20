4f0d243563018278b2587876955d470e62543400
blaine <devnull@localhost>
2008-08-20 01:06:21 +0000

    Fix for expand/collapse of the FCKEditor - via the advanced editor +/- feature.

--- public_html/javascript/storyeditor_fckeditor.js
+++ public_html/javascript/storyeditor_fckeditor.js
@@ -43,21 +43,16 @@
     }
 
     function changeHTMLTextAreaSize(element, option) {
-        var size = 0;
-        var size = document.getElementById(element + '___Frame').height;
-
+        var currentSize = parseInt(document.getElementById(element + '___Frame').style.height);
         if (option == 'larger') {
-            document.getElementById(element + '___Frame').height = +(size) + 50;
-            document.getElementById(element + '___Frame').style.height = +(size) + 50;
-
+            var newsize = currentSize + 50;
         } else if (option == 'smaller') {
-            document.getElementById(element + '___Frame').height = +(size) - 50;
-            document.getElementById(element + '___Frame').style.height = +(size) - 50;
+            var newsize = currentSize - 50;
         }
+        document.getElementById(element + '___Frame').style.height = newsize + 'px';
     }
 
     function changeTextAreaSize(element, option) {
-        var size = 0;
         var size = document.getElementById(element).rows;
         if (option == 'larger') {
             document.getElementById(element).rows = +(size) + 3;
