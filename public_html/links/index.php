6f1cb4cf3ac453c95d91a69f0cb0491cacad59ec
Dirk Haun <dirk@haun-online.de>
2008-11-22 23:23:34 +0100

    Added urlencoded versions of {link_actual_url} and {link_name}

--- public_html/links/index.php
+++ public_html/links/index.php
@@ -344,15 +344,18 @@ function prepare_link_item ($A, &$template)
 {
     global $_CONF, $_USER, $LANG_ADMIN, $LANG_LINKS, $_IMAGE_TYPE, $LANG_DIRECTION;
 
-    $url = COM_buildUrl ($_CONF['site_url']
-                 . '/links/portal.php?what=link&amp;item=' . $A['lid']);
-    $template->set_var ('link_url', $url);
-    $template->set_var ('link_actual_url', $A['url']);
-    $template->set_var ('link_name', stripslashes ($A['title']));
-    $template->set_var ('link_hits', COM_numberFormat ($A['hits']));
-    $template->set_var ('link_description',
-                        nl2br (stripslashes ($A['description'])));
-    $content = stripslashes ($A['title']);
+    $url = COM_buildUrl($_CONF['site_url']
+                        . '/links/portal.php?what=link&amp;item=' . $A['lid']);
+    $template->set_var('link_url', $url);
+    $template->set_var('link_actual_url', $A['url']);
+    $template->set_var('link_actual_url_encoded', urlencode($A['url']));
+    $template->set_var('link_name', stripslashes($A['title']));
+    $template->set_var('link_name_encoded',
+                       urlencode(stripslashes($A['title'])));
+    $template->set_var('link_hits', COM_numberFormat ($A['hits']));
+    $template->set_var('link_description',
+                       nl2br(stripslashes($A['description'])));
+    $content = stripslashes($A['title']);
     $class = 'ext-link';
     if ((!empty($LANG_DIRECTION)) && ($LANG_DIRECTION == 'rtl')) {
         $class .= '-rtl';
