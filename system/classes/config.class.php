689a969e30be69a4cfb3e4ed7e1bb5783d01e646
Dirk Haun <dirk@haun-online.de>
2008-11-23 18:27:55 +0100

    Missing global declaration of $_USER

--- system/classes/config.class.php
+++ system/classes/config.class.php
@@ -576,7 +576,7 @@ class config {
 
     function _UI_perm_denied()
     {
-        global $MESSAGE;
+        global $_USER, $MESSAGE;
 
         $display = COM_siteHeader('menu', $MESSAGE[30])
                  . COM_showMessageText($MESSAGE[29], $MESSAGE[30])
