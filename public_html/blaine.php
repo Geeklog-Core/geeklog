7efd0123f5219193020450bffd487e98d8d7fa5d
blaine <devnull@localhost>
2009-01-18 16:48:19 -0500

    My test file - Blaine

---
+++ public_html/blaine.php
@@ -0,0 +1,12 @@
+<?php
+include ('lib-common.php');
+
+echo COM_siteHeader();
+echo COM_startBlock();
+
+echo '<blockquote>This is a test file - ignore</blockquote>';
+
+echo COM_endBlock();
+echo COM_siteFooter();
+
+?>
