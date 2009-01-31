a58da7ad86474e2a5f09d033634ef540cb7624f0
Dirk Haun <dirk@haun-online.de>
2009-01-31 18:49:22 +0100

    Fixed outdated syntax

--- public_html/lib-common.php
+++ public_html/lib-common.php
@@ -6749,7 +6749,7 @@ function COM_renderWikiText($wikitext)
 
     require_once 'Text/Wiki.php';
 
-    $wiki = &new Text_Wiki();
+    $wiki = new Text_Wiki();
     $wiki->disableRule('wikilink');
     $wiki->disableRule('freelink');
     $wiki->disableRule('interwiki');
