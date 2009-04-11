edd148a7628fea39736a9a587a1f79a0c664a020
Dirk Haun <dirk@haun-online.de>
2009-04-11 17:03:12 +0200

    Make the CIDR check not trip over Mac OS X's use of ::1 for localhost

--- plugins/spamx/IP.Examine.class.php
+++ plugins/spamx/IP.Examine.class.php
@@ -99,16 +99,18 @@ class IP extends BaseCommand {
         // here's our highest int
         $high = $i | (~$mask & 0xFFFFFFFF);
 
-        // now split the ip were checking against up into classes
-        list($a, $b, $c, $d) = explode('.', $iptocheck);
+        // now split the ip we're checking against up into classes
+        $ex = explode('.', $iptocheck);
 
-        // now convert the ip we're checking against to an int
-        $check = ($a << 24) + ($b << 16) + ($c << 8) + $d;
+        if (count($ex) == 4) {
+            // now convert the ip we're checking against to an int
+            $check = ($ex[0] << 24) + ($ex[1] << 16) + ($ex[2] << 8) + $ex[3];
 
-        // if the ip is within the range, including
-        // highest/lowest values, then it's witin the CIDR range
-        if (($check >= $low) && ($check <= $high)) {
-            return true;
+            // if the ip is within the range, including
+            // highest/lowest values, then it's witin the CIDR range
+            if (($check >= $low) && ($check <= $high)) {
+                return true;
+            }
         }
 
         return false;
