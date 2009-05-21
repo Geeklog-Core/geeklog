5db43166a46362435a47da327ceb350424e7481f
Dirk Haun <dirk@haun-online.de>
2009-05-21 09:09:56 +0200

    Make the (deprecated) setPercision method call setPrecision to avoid code duplication

--- system/classes/timer.class.php
+++ system/classes/timer.class.php
@@ -109,7 +109,7 @@ class timerobject {
     */
     function setPercision($num_dec_places)
     {
-        $this->_precision = $num_dec_places;
+        return setPrecision($num_dec_places);
     }
 
 
