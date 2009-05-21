e5f33220062257d8b83faf83a12dd924c274f7f7
Dirk Haun <dirk@haun-online.de>
2009-05-21 20:20:19 +0200

    ... including the $this to make it actually work ...

--- system/classes/timer.class.php
+++ system/classes/timer.class.php
@@ -109,7 +109,7 @@ class timerobject {
     */
     function setPercision($num_dec_places)
     {
-        return setPrecision($num_dec_places);
+        return $this->setPrecision($num_dec_places);
     }
 
 
