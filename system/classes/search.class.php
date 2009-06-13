c00e54aab41b9de74352c33ba2981a34e370feb5
Dirk Haun <dirk@haun-online.de>
2009-06-13 18:04:05 +0200

    Allow searching by topic (without a query string) again (reported by Markus Wollschläger)

--- system/classes/search.class.php
+++ system/classes/search.class.php
@@ -475,7 +475,7 @@ class Search {
 
         // Make sure there is a query string
         // Full text searches have a minimum word length of 3 by default
-        if ((empty($this->_query) && empty($this->_author)) || ($_CONF['search_use_fulltext'] && strlen($this->_query) < 3))
+        if ((empty($this->_query) && empty($this->_author) && empty($this->_topic)) || ($_CONF['search_use_fulltext'] && strlen($this->_query) < 3))
         {
             $retval = '<p>' . $LANG09[41] . '</p>' . LB;
             $retval .= $this->showForm();
