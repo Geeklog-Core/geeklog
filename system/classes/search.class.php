49bdc549285298ef2bebc855922bc08f56c61368
Sami Barakat <devnull@localhost>
2009-06-30 20:48:52 +0100

    Fixed advanced search not using start and end dates, provided by dengen (bug #0000924)

--- system/classes/search.class.php
+++ system/classes/search.class.php
@@ -432,7 +432,7 @@ class Search {
                 $DE = explode($delim, $this->_dateEnd);
                 $startdate = mktime(0,0,0,$DS[1],$DS[2],$DS[0]);
                 $enddate = mktime(23,59,59,$DE[1],$DE[2],$DE[0]);
-                $sql .= "AND (UNIX_TIMESTAMP(c.date) BETWEEN '$startdate' AND '$enddate') ";
+                $sql .= "AND (UNIX_TIMESTAMP(c.date) BETWEEN '{$this->_dateStart}' AND '{$this->_dateEnd}') ";
             }
         }
         if (!empty($this->_topic)) {
