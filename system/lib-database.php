042f27671a9cc986ddf5abf0c0fc2b45596ff18b
stan <yankees26an@gmail.com>
2009-08-07 12:35:33 -0400

    fixed DB_getItem

--- system/lib-database.php
+++ system/lib-database.php
@@ -265,13 +265,32 @@ function DB_delete($table,$id,$value,$return_page='')
 */
 function DB_getItem($table,$what,$selection='')
 {
-    if (!empty($selection)) {
-        $result = DB_query("SELECT $what FROM $table WHERE $selection");
-    } else {
-        $result = DB_query("SELECT $what FROM $table");
+    global $_DB_dbms;
+    if($_DB_dbms=='mysql')
+    {
+        if (!empty($selection)) {
+            $result = DB_query("SELECT $what FROM $table WHERE $selection");
+        } else {
+            $result = DB_query("SELECT $what FROM $table");
+        }
+        $ITEM = DB_fetchArray($result, true);
+        return $ITEM[0];
     }
-    $ITEM = DB_fetchArray($result, true);
-    return $ITEM[0];
+    elseif($_DB_dbms=='pgsql')
+    {
+        $result = DB_query("SELECT check_table('$table', 'public');");
+        $row=DB_fetchArray($result);
+        if(!empty($row[0]))
+        {     //table exists
+             if(!empty($selection)) {$result = DB_query("SELECT $what FROM $table WHERE $selection");}
+             else {$result = DB_query("SELECT $what FROM $table");}
+             
+             $ITEM = DB_fetchArray($result, true);
+             return $ITEM[0];   
+        }
+    }
+    else{ COM_errorLog ('Unknown DB selected', 3);}
+    
 }
 
 /**
