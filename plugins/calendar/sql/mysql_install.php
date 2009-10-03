2b8a47026400aef753719088cfd2ce40e50f61ce
Dirk Haun <dirk@haun-online.de>
2009-10-03 11:46:54 +0200

    Keep track of the user id for submitted events (bug #0000993)

--- plugins/calendar/sql/mysql_install.php
+++ plugins/calendar/sql/mysql_install.php
@@ -82,6 +82,7 @@ CREATE TABLE {$_TABLES['eventsubmission']} (
   event_type varchar(40) NOT NULL default '',
   timestart time default NULL,
   timeend time default NULL,
+  owner_id mediumint(8) unsigned NOT NULL default '1',
   PRIMARY KEY  (eid)
 ) TYPE=MyISAM
 ";
@@ -116,7 +117,7 @@ CREATE TABLE {$_TABLES['personal_events']} (
 ) TYPE=MyISAM
 ";
 
-$_SQL[] = "INSERT INTO {$_TABLES['eventsubmission']} (eid, title, description, location, datestart, dateend, url, allday, zipcode, state, city, address2, address1, event_type, timestart, timeend) VALUES ('2008050110130162','Installed the Calendar plugin','Today, you successfully installed the Calendar plugin.','Your webserver',CURDATE(),CURDATE(),'http://www.geeklog.net/',1,NULL,NULL,NULL,NULL,NULL,'',NULL,NULL)";
+$_SQL[] = "INSERT INTO {$_TABLES['eventsubmission']} (eid, title, description, location, datestart, dateend, url, allday, zipcode, state, city, address2, address1, event_type, timestart, timeend, owner_id) VALUES ('2008050110130162','Installed the Calendar plugin','Today, you successfully installed the Calendar plugin.','Your webserver',CURDATE(),CURDATE(),'http://www.geeklog.net/',1,NULL,NULL,NULL,NULL,NULL,'',NULL,NULL,1)";
 
 $_SQL[] = "INSERT INTO {$_TABLES['blocks']} (is_enabled, name, type, title, tid, blockorder, content, onleft, phpblockfn, owner_id, group_id, perm_owner, perm_group) VALUES (1,'events_block','phpblock','Events','all',4,'',1,'phpblock_calendar',{$_USER['uid']},#group#,3,3)";
 
