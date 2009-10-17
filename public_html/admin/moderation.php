375d1263d6e8327a7eacd925ee57acaaab2ad5d6
Dirk Haun <dirk@haun-online.de>
2009-10-17 13:11:32 +0200

    Fixed a long-standing quirk of the submission handling where the "Submissions" entry in the Admins Block wasn't updated after accepting / rejecting a submission

--- public_html/admin/moderation.php
+++ public_html/admin/moderation.php
@@ -742,24 +742,28 @@ function security_check_reminder()
 // MAIN
 
 $display = '';
-$display .= COM_siteHeader ('menu', $LANG29[34]);
-$display .= COM_showMessageFromParameter();
 
-if (isset ($_POST['mode']) && ($_POST['mode'] == 'moderation') && SEC_checkToken()) {
+if (isset($_POST['mode']) && ($_POST['mode'] == 'moderation') &&
+        SEC_checkToken()) {
     $action = array();
     if (isset($_POST['action'])) {
         $action = $_POST['action'];
     }
     if ($_POST['type'] == 'user') {
-        $display .= moderateusers($_POST['id'], $action,
-                                  COM_applyFilter($_POST['count'], true));
+        $mod_result = moderateusers($_POST['id'], $action,
+                                    COM_applyFilter($_POST['count'], true));
     } else {
-        $display .= moderation($_POST['id'], $action, $_POST['type'],
-                               COM_applyFilter ($_POST['count'], true));
+        $mod_result = moderation($_POST['id'], $action, $_POST['type'],
+                                 COM_applyFilter($_POST['count'], true));
     }
+    $display .= COM_siteHeader('menu', $LANG29[34])
+             .  COM_showMessageFromParameter()
+             .  $mod_result;
 } else {
-    $display .= security_check_reminder();
-    $display .= commandcontrol(SEC_createToken());
+    $display .= COM_siteHeader('menu', $LANG29[34])
+             .  COM_showMessageFromParameter()
+             .  security_check_reminder()
+             .  commandcontrol(SEC_createToken());
 }
 
 $display .= COM_siteFooter();
