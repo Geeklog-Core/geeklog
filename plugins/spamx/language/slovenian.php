8b5068ab5774a6741d7834a3618a3a475a397ba8
Dirk Haun <dirk@haun-online.de>
2010-05-08 20:32:27 +0200

    New and updated Slovenian language files for the Spam-X and XMLSitemap plugins, provided by Mateja B.

--- plugins/spamx/language/slovenian.php
+++ plugins/spamx/language/slovenian.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * File: # slovenian.php - version 1.4.1
+ * File: # slovenian.php - version 1.7
  * This is the slovenian language file for the Geeklog Spam-X plugin
- * language file for geeklog version 1.4.1 beta by mb
- * gape@gape.org ; za pripombe, predloge ipd ... piši na email
+ * language file for geeklog version 1.7 by Mateja B.
+ * gape@gape.org ; za pripombe, predloge ipd. piši na e-naslov
 
  * 
  * Copyright (C) 2004-2006 by the following authors:
@@ -12,30 +12,29 @@
  * 
  * Licensed under GNU General Public License
  *
- * $Id: slovenian.php,v 1.4 2008/05/02 15:08:10 dhaun Exp $
  */
 
 global $LANG32;
 
 $LANG_SX00 = array(
     'inst1' => '<p>Èe storiš to, potem si bodo drugi ',
-    'inst2' => ' lahko ogledali in uvozili tvojo èrno listo, in lahko bomo ustvarili bolj uèinkovito ',
-    'inst3' => 'razvršèeno podatkovno bazo.</p><p>Èe si oddal/a svojo spletno stran in se odloèil/a, da ne želiš, da tvoja spletna stran ostane na listi ',
+    'inst2' => ' lahko ogledali in uvozili tvojo èrno listo, in lahko bomo ustvarili uèinkovitejšo ',
+    'inst3' => 'razvršèeno podatkovno bazo.</p><p>Èe si oddal/a svojo spletno stran in noèeš, da tvoja spletna stran ostane na listi, ',
     'inst4' => 'to sporoèi prek e-pošte na naslov <a href="mailto:spamx@pigstye.net">spamx@pigstye.net</a>. ',
     'inst5' => 'Vse zahteve bodo spoštovane.',
     'submit' => 'Oddaj',
-    'subthis' => 'this info to Spam-X Central Database',
-    'secbut' => 'Ta drugi gumb ustvari združeno vsebino v zapisu rdf, da lahko drugi uvozijo tvojo listo.',
+    'subthis' => 'te informacije v centralno podatkovno bazo Spam-X',
+    'secbut' => 'Ta, drugi gumb ustvari združeno vsebino v zapisu rdf, da lahko drugi uvozijo tvojo listo.',
     'sitename' => 'Ime strani: ',
     'URL' => 'URL v seznam Spam-X : ',
     'RDF' => 'RDF url: ',
-    'impinst1a' => 'Before you use the Spam-X comment Spam blocker facility to view and import personal Blacklists from other',
-    'impinst1b' => ' sites, I ask that you press the following two buttons. (You have to press the last one.)',
-    'impinst2' => 'This first submits your website to the Gplugs/Spam-X site so that it can be added to the master list of ',
-    'impinst2a' => 'sites sharing their blacklists. (Note: if you have multiple sites you might want to designate one as the ',
-    'impinst2b' => 'master and only submit its name. This will allow you to update your sites easily and keep the list smaller.) ',
-    'impinst2c' => 'After you press the Submit button, press [back] on your browser to return here.',
-    'impinst3' => 'The Following values will be sent: (you can edit them if they are wrong).',
+    'impinst1a' => 'Preden uporabiš pripomoèek Spam-X comment Spam blocker, da pogledaš in uvoziš osebne èrne list z drugih',
+    'impinst1b' => ' strani, prosim klikni naslednja dva gumba. (Klikniti moraš zadnjega.)',
+    'impinst2' => 'To najprej odda tvojo spletno stran na stran Gplugs/Spam-X, da bo lahko dodana na glavno listo ',
+    'impinst2a' => 'strani, ki si delijo èrne liste. (Opomba: Èe imaš veè strani, hoèeš morda eno doloèiti kot ',
+    'impinst2b' => 'glavno in oddati samo njeno ime. To ti bo omogoèilo preprosto posodobitev svojih strani in ohraniti krajšo listo.) ',
+    'impinst2c' => 'Po kliku na gumb Oddaj se z gumbom Nazaj v brskalniku vrni spet sem.',
+    'impinst3' => 'Poslane bodo naslednje vrednosti: (èe so napaène, jih uredi).',
     'availb' => 'Razpoložljive èrne liste',
     'clickv' => 'Klikni za ogled èrne liste',
     'clicki' => 'Klikni za uvoz èrne liste',
@@ -44,22 +43,22 @@ $LANG_SX00 = array(
     'add1' => 'Dodani ',
     'add2' => ' vpisi s èrne liste uporabnika ',
     'add3' => '.',
-    'adminc' => 'Upravniški ukazi:',
+    'adminc' => 'Skrbniški ukazi:',
     'mblack' => 'Moja èrna lista:',
     'rlinks' => 'Sorodne povezave:',
     'e3' => ' Za dodajanje besed iz Geeklogovega cenzurnega seznama pritisni gumb:',
     'addcen' => 'Dodaj cenzurni seznam',
     'addentry' => 'Dodaj vpis',
-    'e1' => 'Da izbrišeš vpis, ga klikni.',
-    'e2' => 'Da dodaš vpis, ga vnesi v okence in klikni Dodaj. Vnosi lahko uporabljajo  full Perl Regular Expressions.',
+    'e1' => 'Da vpis izbrišeš, ga klikni.',
+    'e2' => 'Da vpis dodaš, ga vnesi v okence in klikni Dodaj. Vnosi lahko uporabljajo polne izraze Perl Regular.',
     'pblack' => 'Osebna èrna lista Spam-X',
     'conmod' => 'Konfiguriraj Spam-X Module Usage',
     'acmod' => 'Akcijski moduli Spam-X ',
     'exmod' => 'Preiskovalni moduli Spam-X ',
     'actmod' => 'Aktivni moduli',
     'avmod' => 'Razpoložljivi moduli',
-    'coninst' => '<hr' . XHTML . '>Klikni na aktivni modul, da ga odstraniš, klikni na Razpoložljivi modul, da ga dodaš.<br' . XHTML . '>Moduli are executed in order presented.',
-    'fsc' => 'Found Spam Post matching ',
+    'coninst' => '<hr' . XHTML . '>Klikni na aktivni modul, da ga odstraniš, klikni na Razpoložljivi modul, da ga dodaš.<br' . XHTML . '>Moduli se izvajajo v predloženem vrstnem redu.',
+    'fsc' => 'Najden Spam Post, ki se ujema z ',
     'fsc1' => ' objavil uporabnik ',
     'fsc2' => ' z IP-ja ',
     'uMTlist' => 'Posodobi èrno listo MT',
@@ -74,93 +73,93 @@ $LANG_SX00 = array(
     'logcleared' => '- Datoteka Spam-X Log poèišèena',
     'plugin' => 'Vtiènik',
     'access_denied' => 'Dostop zavrnjen',
-    'access_denied_msg' => 'Dostop do te strani imajo samo korenski uporabniki.  Tvoje uporabniško ime in IP sta zapisana.',
+    'access_denied_msg' => 'Dostop do te strani imajo samo korenski (root) uporabniki. Tvoje uporabniško ime in IP sta zapisana.',
     'admin' => 'Upravljanje vtiènikov',
     'install_header' => 'Namesti/deinstaliraj vtiènik',
     'installed' => 'Vtiènik je namešèen',
     'uninstalled' => 'Vtiènik ni namešèen',
     'install_success' => 'Namestitev uspešna',
-    'install_failed' => 'Namestitev ni uspela -- See your error log to find out why.',
-    'uninstall_msg' => 'Plugin Successfully Uninstalled',
-    'install' => 'Install',
-    'uninstall' => 'UnInstall',
-    'warning' => 'Warning! Plugin is still Enabled',
-    'enabled' => 'Disable plugin before uninstalling.',
-    'readme' => 'STOP! Before you press install please read the ',
+    'install_failed' => 'Namestitev ni uspela -- Za razloge poglej error log.',
+    'uninstall_msg' => 'Vtiènik uspešno odstranjen',
+    'install' => 'Namesti',
+    'uninstall' => 'Odstrani',
+    'warning' => 'Opozorilo! Vtiènik je še vedno omogoèen',
+    'enabled' => 'Pred odstranitvijo vtiènik onemogoèi.',
+    'readme' => 'STOP! Preden zaèeneš namestitev, prosim preberi ',
     'installdoc' => 'Install Document.',
-    'spamdeleted' => 'Deleted Spam Post',
-    'foundspam' => 'Found Spam Post matching ',
-    'foundspam2' => ' posted by user ',
-    'foundspam3' => ' from IP ',
-    'deletespam' => 'Delete Spam',
-    'numtocheck' => 'Number of Comments to check',
-    'note1' => '<p>Note: Mass Delete is intended to help you when you are hit by',
-    'note2' => ' comment spam and Spam-X does not catch it.  <ul><li>First find the link(s) or other ',
-    'note3' => 'identifiers of this spam comment and add it to your personal blacklist.</li><li>Then ',
-    'note4' => 'come back here and have Spam-X check the latest comments for spam.</li></ul><p>Comments ',
-    'note5' => 'are checked from newest comment to oldest -- checking more comments ',
-    'note6' => 'requires more time for the check.</p>',
-    'masshead' => '<hr' . XHTML . '><h1 align="center">Mass Delete Spam Comments</h1>',
-    'masstb' => '<hr' . XHTML . '><h1 align="center">Mass Delete Trackback Spam</h1>',
-    'comdel' => ' comments deleted.',
-    'initial_Pimport' => '<p>Personal Blacklist Import"',
-    'initial_import' => 'Initial MT-Blacklist Import',
-    'import_success' => '<p>Successfully imported %d blacklist entries.',
-    'import_failure' => '<p><strong>Error:</strong> No entries found.',
-    'allow_url_fopen' => '<p>Sorry, your webserver configuration does not allow reading of remote files (<code>allow_url_fopen</code> is off). Please download the blacklist from the following URL and upload it into Geeklog\'s "data" directory, <tt>%s</tt>, before trying again:',
-    'documentation' => 'Spam-X Plugin Documentation',
-    'emailmsg' => "A new spam post has been submitted at \"%s\"\nUser UID: \"%s\"\n\nContent:\"%s\"",
-    'emailsubject' => 'Spam post at %s',
+    'spamdeleted' => 'Izbrisan Spam Post',
+    'foundspam' => 'Najden Spam Post, ki se ujema z ',
+    'foundspam2' => ' objavil uporabnik ',
+    'foundspam3' => ' z IP ',
+    'deletespam' => 'Izbriši Spam',
+    'numtocheck' => 'Število komentarjev za preverbo',
+    'note1' => '<p>Opomba: Masovni izbris je namenjen kot pomoè v primeru, ko te doleti',
+    'note2' => ' spam komenratjev in ga Spam-X ne ujame.  <ul><li>najprej najdi povezavo/povezave ali druge ',
+    'note3' => 'identifikatorje tega spama komentarjev in jo/jih dodaj na osebno èrno listo.</li><li>Nato ',
+    'note4' => 'se vrni sem in s Spam-X preveri zadnje komentarje za morebiten spam.</li></ul><p>Komentarji ',
+    'note5' => 'so preverjeni od najnovejšega do najstarejšega -- preveritev veè komentarjev ',
+    'note6' => 'zahteva daljši èas preverjanja.</p>',
+    'masshead' => '<hr' . XHTML . '><h1 align="center">Masovni izbris spam komentarjev</h1>',
+    'masstb' => '<hr' . XHTML . '><h1 align="center">Masovni izbris spam trackbackov</h1>',
+    'comdel' => ' komentarjev izbrisanih.',
+    'initial_Pimport' => '<p>Uvoz osebne èrne liste"',
+    'initial_import' => 'Zaèetni uvoz MT-èrne liste',
+    'import_success' => '<p>Število uspešno uvoženih vnosov èrne liste: %d .',
+    'import_failure' => '<p><strong>Error:</strong> Ni najdenih vnosov.',
+    'allow_url_fopen' => '<p>Žal konfiguracija tvojega spletnega strežnika ne dopušèa branja oddaljenih datotek (<code>allow_url_fopen</code> je izkljuèen). Prenesi prosim èrno listo s sledeèega URL in jo naloži v Geeklogov "podatkovni" direktorij, <tt>%s</tt>, preden poskusiš ponovno:',
+    'documentation' => 'Dokumentacija vtiènika Spam-X',
+    'emailmsg' => "Nova spam objava je bila oddana pri \"%s\"\nUser UID: \"%s\"\n\nContent:\"%s\"",
+    'emailsubject' => 'Spam objava pri %s',
     'ipblack' => 'Spam-X IP Blacklist',
     'ipofurlblack' => 'Spam-X IP of URL Blacklist',
     'headerblack' => 'Spam-X HTTP Header Blacklist',
-    'headers' => 'Request headers:',
-    'stats_headline' => 'Spam-X Statistics',
-    'stats_page_title' => 'Blacklist',
-    'stats_entries' => 'Entries',
-    'stats_mtblacklist' => 'MT-Blacklist',
-    'stats_pblacklist' => 'Personal Blacklist',
-    'stats_ip' => 'Blocked IPs',
-    'stats_ipofurl' => 'Blocked by IP of URL',
-    'stats_header' => 'HTTP headers',
-    'stats_deleted' => 'Posts deleted as spam',
+    'headers' => 'Zahtevaj glave:',
+    'stats_headline' => 'Spam-X statistika',
+    'stats_page_title' => 'Èrna lista',
+    'stats_entries' => 'Vnosi',
+    'stats_mtblacklist' => 'MT-èrna lista',
+    'stats_pblacklist' => 'Osebna èrna lista',
+    'stats_ip' => 'Blokirani IP-ji',
+    'stats_ipofurl' => 'Blokirano z IP URL-ja',
+    'stats_header' => 'HTTP glave',
+    'stats_deleted' => 'Objave, izbrisane kot spam',
     'plugin_name' => 'Spam-X',
-    'slvwhitelist' => 'SLV Whitelist'
+    'slvwhitelist' => 'SLV bela lista'
 );
 
 // Define Messages that are shown when Spam-X module action is taken
-$PLG_spamx_MESSAGE128 = 'Spam detected. Post was deleted.';
-$PLG_spamx_MESSAGE8 = 'Spam detected. Email sent to admin.';
+$PLG_spamx_MESSAGE128 = 'Odkrit spam. Objava je bila izbrisana.';
+$PLG_spamx_MESSAGE8 = 'Odkrit spam. E-pošta poslana skrbniku.';
 
 // Messages for the plugin upgrade
-$PLG_spamx_MESSAGE3001 = 'Plugin upgrade not supported.';
+$PLG_spamx_MESSAGE3001 = 'Nadgradnja vtiènika ni podprta.';
 $PLG_spamx_MESSAGE3002 = $LANG32[9];
 
 // Localization of the Admin Configuration UI
 $LANG_configsections['spamx'] = array(
     'label' => 'Spam-X',
-    'title' => 'Spam-X Configuration'
+    'title' => 'Konfiguracija Spam-X'
 );
 
 $LANG_confignames['spamx'] = array(
-    'action' => 'Spam-X Actions',
-    'notification_email' => 'Notification Email',
-    'logging' => 'Enable Logging',
+    'action' => 'Spam-X dejanja',
+    'notification_email' => 'Obvestilna e-pošta',
+    'logging' => 'Omogoèi shranjevanje v log',
     'timeout' => 'Timeout'
 );
 
 $LANG_configsubgroups['spamx'] = array(
-    'sg_main' => 'Main Settings'
+    'sg_main' => 'Glavne nastavitve'
 );
 
 $LANG_fs['spamx'] = array(
-    'fs_main' => 'Spam-X Main Settings'
+    'fs_main' => 'Glavne nastavitve Spam-X'
 );
 
 // Note: entries 0, 1, 9, and 12 are the same as in $LANG_configselects['Core']
 $LANG_configselects['spamx'] = array(
-    0 => array('True' => 1, 'False' => 0),
-    1 => array('True' => true, 'False' => false)
+    0 => array('Da' => 1, 'Ne' => 0),
+    1 => array('Da' => 'velja', 'Ne' => 'ne velja')
 );
 
 ?>
---
+++ plugins/xmlsitemap/language/slovenian_utf-8.php
@@ -0,0 +1,71 @@
+<?php
+
+/* Reminder: always indent with 4 spaces (no tabs). */
+// +---------------------------------------------------------------------------+
+// | XMLSitemap Plugin                                                         |
+// +---------------------------------------------------------------------------+
+// | slovenian_utf-8.php                                                       |
+// +---------------------------------------------------------------------------+
+// | Copyright (C) 2009 by the following authors:                              |
+// |                                                                           |
+// | Authors: Kenji ITO         - geeklog AT mystral-kk DOT net                |
+// |          Dirk Haun         - dirk AT haun-online DOT de                   |
+// +---------------------------------------------------------------------------|
+// | This program is free software; you can redistribute it and/or             |
+// | modify it under the terms of the GNU General Public License               |
+// | as published by the Free Software Foundation; either version 2            |
+// | of the License, or (at your option) any later version.                    |
+// |                                                                           |
+// | This program is distributed in the hope that it will be useful,           |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+// | GNU General Public License for more details.                              |
+// |                                                                           |
+// | You should have received a copy of the GNU General Public License         |
+// | along with this program; if not, write to the Free Software               |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA|
+// |                                                                           |
+// +---------------------------------------------------------------------------|
+
+global $LANG32;
+
+$LANG_XMLSMAP = array(
+    'plugin' => 'XML kazalo strani (Sitemap)',
+    'admin' => 'Skrbnikovo XML kazalo strani'
+);
+
+// Localization of the Admin Configuration UI
+$LANG_configsections['xmlsitemap'] = array(
+    'label' => 'XML kazalo strani (Sitemap)',
+    'title' => 'Konfiguracija za XML kazalo strani'
+);
+
+$LANG_confignames['xmlsitemap'] = array(
+    'sitemap_file' => 'Ime kazala strani',
+    'mobile_sitemap_file' => 'Mobilno ime kazala strani',
+    'types' => 'Vsebina kazala strani',
+    'exclude' => 'Vtièniki za ikljuèitev iz kazala strani',
+    'priorities' => '',
+    'frequencies' => ''
+);
+
+$LANG_configsubgroups['xmlsitemap'] = array(
+    'sg_main' => 'Glavne nastavitve'
+);
+
+$LANG_fs['xmlsitemap'] = array(
+    'fs_main' => ' Glavne nastavitve XML kazala strani',
+    'fs_pri' => 'Prioriteta (prednastavljena = 0.5, najnižja = 0.0, najvišja = 1.0)',
+    'fs_freq' => 'Pogostost posodobitev'
+);
+
+// Note: entries 0, 1, 9, and 12 are the same as in $LANG_configselects['Core']
+$LANG_configselects['xmlsitemap'] = array(
+    0 => array('Da' => 1, 'Ne' => 0),
+    1 => array('Da' => 'velja', 'Ne' => 'ne velja'),
+    9 => array('Naprej na stran' => 'item', 'Prikaži seznam' => 'list', 'Prikaži vstopno stran' => 'home', 'Prikaži skrbnikovo stran' => 'admin'),
+    12 => array('Ni dostopa' => 0, 'Samo za branje' => 2, 'Branje-pisanje' => 3),
+    20 => array('vedno' => 'always', 'ob uri' => 'hourly', 'dnevno' => 'daily', 'tedensko' => 'weekly', 'meseèno' => 'monthly', 'letno' => 'yearly', 'nikoli' => 'never')
+);
+
+?>
