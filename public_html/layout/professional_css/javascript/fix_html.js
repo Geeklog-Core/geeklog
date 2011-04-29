32bf7fc405558d492265d3b46a987f03822ef04f
Tom <websitemaster@cogeco.net>
2011-04-29 19:20:04 -0400

    Fixed IE 9 version number

--- public_html/layout/professional_css/javascript/fix_html.js
+++ public_html/layout/professional_css/javascript/fix_html.js
@@ -1,29 +1,27 @@
 function FixHTML( leftblocksID, centerblocksID, rightblocksID ) {
-  var ua = navigator.userAgent.toLowerCase();
-  var is_old_ie = (ua.indexOf("msie") != -1) && (ua.indexOf("msie 8") == -1) && (ua.indexOf("msie 9.0") == -1) && (ua.indexOf("opera") == -1);
+    var ua = navigator.userAgent.toLowerCase();
+    var is_old_ie = (ua.indexOf("msie") != -1) && (ua.indexOf("msie 8") == -1) && (ua.indexOf("msie 9") == -1) && (ua.indexOf("opera") == -1);
+    
+    var leftblocks = document.getElementById(leftblocksID);
+    var centerblocks = document.getElementById(centerblocksID);
+    var rightblocks = document.getElementById(rightblocksID);
 
-  var leftblocks = document.getElementById(leftblocksID);
-  var centerblocks = document.getElementById(centerblocksID);
-  var rightblocks = document.getElementById(rightblocksID);
-
-
-
-  if ( document.body.getAttribute('class') || document.body.getAttribute('className') ) {
-    var classValue = 'left-center-right';
-
-    /* Check HTML id attribute. */
-    if ( leftblocks && centerblocks && !rightblocks ) classValue = 'left-center';
-    if ( !leftblocks && centerblocks && rightblocks ) classValue = 'center-right';
-    if ( !leftblocks && centerblocks && !rightblocks ) classValue = 'center';
-
-    /* Set js_on to body class attribute  */
-    classValue += ' js_on';
-
-    /* Set body class attribute by HTML structure. */
-    if ( is_old_ie ) {  /* IE8, IE7, IE6 */
-      document.body.setAttribute('className', classValue);
-    } else {  /* Gecko, Opera, Safari, IE9 and other */
-      document.body.setAttribute('class', classValue);
+    if ( document.body.getAttribute('class') || document.body.getAttribute('className') ) {
+        var classValue = 'left-center-right';
+        
+        /* Check HTML id attribute. */
+        if ( leftblocks && centerblocks && !rightblocks ) classValue = 'left-center';
+        if ( !leftblocks && centerblocks && rightblocks ) classValue = 'center-right';
+        if ( !leftblocks && centerblocks && !rightblocks ) classValue = 'center';
+        
+        /* Set js_on to body class attribute  */
+        classValue += ' js_on';
+        
+        /* Set body class attribute by HTML structure. */
+        if ( is_old_ie ) {  /* IE8, IE7, IE6 */
+            document.body.setAttribute('className', classValue);
+        } else {  /* Gecko, Opera, Safari, IE9 and other */
+            document.body.setAttribute('class', classValue);
+        }
     }
-  }
 }
