7c562926d8e065f55c754a4ba7c45cac3cdd34f1
Dirk Haun <dirk@haun-online.de>
2011-04-30 14:54:55 +0200

    Create a nice username for LinkedIn OAuth users

--- system/classes/oauth/linkedin.auth.class.php
+++ system/classes/oauth/linkedin.auth.class.php
@@ -50,7 +50,7 @@ class linkedinConsumer extends OAuthConsumerBaseClass {
 
     protected function _getCreateUserInfo($info) {
         $users = array(
-            'loginname'      => $info->id,
+            'loginname'      => $info->{'first-name'} . $info->{'last-name'},
             'email'          => '',
             'passwd'         => '',
             'passwd2'        => '',
@@ -60,6 +60,7 @@ class linkedinConsumer extends OAuthConsumerBaseClass {
             'remoteservice'  => 'oauth.linkedin',
             'remotephoto'    => $info->{'picture-url'},
         );
+        $users['loginname'] = str_replace(' ', '', $users['loginname']);
         return $users;
     }
 
