998a5ae19b741476b65c261c8bdd2fcee1933c76
Rouslan Placella <rouslan@placella.com>
2012-03-22 22:28:05 +0000

    newpro: fixed login form display for RTL languages

--- system/lib-security.php
+++ system/lib-security.php
@@ -1801,7 +1801,7 @@ function SEC_loginRequiredForm()
 */
 function SEC_loginForm($use_config = array())
 {
-    global $_CONF, $LANG01, $LANG04, $_SCRIPTS;
+    global $_CONF, $LANG01, $LANG04, $_SCRIPTS, $LANG_DIRECTION;
 
     $retval = '';
 
@@ -1833,6 +1833,7 @@ function SEC_loginForm($use_config = array())
 
     $loginform->set_var('start_block_loginagain',
                         COM_startBlock($config['title']));
+    $loginform->set_var('lang_direction', $LANG_DIRECTION);
     $loginform->set_var('lang_message', $config['message']);
     if ($config['no_newreg_link'] || $_CONF['disable_new_user_registration']) {
         $loginform->set_var('lang_newreglink', '');
