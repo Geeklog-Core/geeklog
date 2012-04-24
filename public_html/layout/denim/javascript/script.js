7604d1fc15585a333b2b686088f6153386e7e681
dengen <devnull@localhost>
2012-04-24 10:17:06 +0900

    Denim: because function Com_createHTMLDocument is adopted, flag 'js_off' or 'js_on' is no longer needed.

--- public_html/layout/denim/javascript/script.js
+++ public_html/layout/denim/javascript/script.js
@@ -1,48 +1,37 @@
-function FixHTML( leftblocksID, centerblocksID, rightblocksID ) {
+function FixHTML(leftblocksID, centerblocksID, rightblocksID) {
     var ua = navigator.userAgent.toLowerCase();
-    var is_old_ie = (ua.indexOf("msie") != -1) && (ua.indexOf("msie 8") == -1) && (ua.indexOf("msie 9") == -1) && (ua.indexOf("opera") == -1);
-    
-    var leftblocks = document.getElementById(leftblocksID);
-    var centerblocks = document.getElementById(centerblocksID);
-    var rightblocks = document.getElementById(rightblocksID);
+    var is_old_ie = (ua.indexOf("msie") != -1) && (ua.indexOf("msie 8") == -1) &&
+                    (ua.indexOf("msie 9") == -1) && (ua.indexOf("opera") == -1);
+    // Set class attribute name
+    // 'class'     for Gecko, Opera, Safari, IE9 and other
+    // 'className' for IE8, IE7, IE6
+    var classattr = (is_old_ie) ? 'className' : 'class';
 
-    if ( document.body.getAttribute('class') == '' || !document.body.getAttribute('className') == '' ) {
-        var classValue = 'left-center-right';
-        
-        /* Check HTML id attribute. */
-        if ( leftblocks && centerblocks && !rightblocks ) classValue = 'left-center';
-        if ( !leftblocks && centerblocks && rightblocks ) classValue = 'center-right';
-        if ( !leftblocks && centerblocks && !rightblocks ) classValue = 'center';
-        
-        /* Set js_on to body class attribute  */
-        classValue += ' js_on';
-        
-        /* Set body class attribute by HTML structure. */
-        if ( is_old_ie ) {  /* IE8, IE7, IE6 */
-            document.body.setAttribute('className', classValue);
-        } else {  /* Gecko, Opera, Safari, IE9 and other */
-            document.body.setAttribute('class', classValue);
-        }
-    }
+    if (document.body.getAttribute(classattr) != '') return;
+
+    var leftblocks   = document.getElementById(leftblocksID);
+    var centerblocks = document.getElementById(centerblocksID);
+    var rightblocks  = document.getElementById(rightblocksID);
+
+    // Check HTML id attribute
+    var classValue = 'left-center-right';
+    if (leftblocks  && centerblocks && !rightblocks) classValue = 'left-center';
+    if (!leftblocks && centerblocks && rightblocks ) classValue = 'center-right';
+    if (!leftblocks && centerblocks && !rightblocks) classValue = 'center';
+    
+    // Set body class attribute by HTML structure
+    document.body.setAttribute(classattr, classValue);
 }
 
 function delconfirm() {
-  if ( confirm("Delete this?") ) {
-    return true;
-  } else {
-    return false;
-  }
+  return (confirm("Delete this?")) ? true : false;
 }
 
 function postconfirm() {
-  if ( confirm("Send this?") ) {
-    return true;
-  } else {
-    return false;
-  }
+  return (confirm("Send this?")) ? true : false;
 }
 
-$(function () {
+$(function() {
   $('#navigation_ul').tinyNav({
     active: 'selected'
   });
