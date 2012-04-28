301c5ace92c00ca53bf5f4b875ba0f7e419ebc0b
dengen <devnull@localhost>
2012-04-29 00:03:55 +0900

    Modified for small devices such as iPhone But still not complete

--- public_html/javascript/fix_tooltips.js
+++ public_html/javascript/fix_tooltips.js
@@ -6,20 +6,20 @@ var fix_tooltips = function() {
     var viewport_width  = parseInt($w.width());
     // fix each tooltip
     $('a.gl-tooltip').each(function() {
-         // cache the tooltip element
+        // cache the tooltip element
         var $s = $(this).find('span');
         // estimate the bottom and right coordinates of the tooltip
         var tooltip_bottom  = parseInt($(this).offset().top + $s.outerHeight() - $w.scrollTop() + 30);
-        var tooltip_right   = parseInt($(this).offset().left + $s.outerWidth() - $w.scrollLeft() + 30);
+        var tooltip_right   = parseInt($(this).offset().left + $s.outerWidth() - $w.scrollLeft() + 40);
         // move the element around as necessary
         if (tooltip_bottom >= viewport_height) {
-            var top = parseInt(($s.outerHeight() + 22) * -1) + 'px';
+            var top = parseInt(($s.outerHeight() + 10) * -1) + 'px';
             $s.css('top', top);
         } else {
             $s.css('top', '2.2em');
         }
         if (tooltip_right >= viewport_width) {
-            var left = parseInt($s.width() * -1) + 'px';
+            var left = parseInt(viewport_width - tooltip_right + 30) + 'px';
             $s.css('left', left);
         } else {
             $s.css('left', '2.2em');
