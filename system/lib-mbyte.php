092f635e6c39fac9f75b29a5d2007b32c52f9fc1
Dirk Haun <dirk@haun-online.de>
2012-07-02 21:15:07 +0200

    backing out changeset 4ef61b9b61ed since it broke some unit tests

--- system/lib-mbyte.php
+++ system/lib-mbyte.php
@@ -224,8 +224,7 @@ function MBYTE_eregi($pattern, $str, $regs = NULL)
     if ($mb_enabled) {
         $result = mb_eregi($pattern, $str, $regs);
     } else {
-        $pattern = addcslashes($pattern, "/");
-        $result = preg_match("/$pattern/i", $str, $regs);
+        $result = eregi($pattern, $str, $regs);
     }
 
     return $result;
@@ -241,8 +240,7 @@ function MBYTE_eregi_replace($pattern, $replace, $str)
     if ($mb_enabled) {
         $result = mb_eregi_replace($pattern, $replace, $str);
     } else {
-        $pattern = addcslashes($pattern, "/");
-        $result = preg_replace("/$pattern/i", $replace, $str);
+        $result = eregi_replace($pattern, $replace, $str);
     }
 
     return $result;
