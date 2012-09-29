1dd17d388a81de6f70756c10526ff3fc3cbd8f0e
dengen <devnull@localhost>
2012-09-29 18:42:23 +0900

    Fixed bug set an invalid value to a template variable in the profiles.php

--- public_html/profiles.php
+++ public_html/profiles.php
@@ -203,6 +203,8 @@ function contactform ($uid, $cc = false, $subject = '', $message = '')
 
             if ($cc) {
                 $cc = ' checked="checked"';
+            } else {
+                $cc = '';
             }
             $retval = COM_startBlock($LANG08[10] . ' ' . $displayname);
             $mail_template = COM_newTemplate($_CONF['path_layout'] . 'profiles');
