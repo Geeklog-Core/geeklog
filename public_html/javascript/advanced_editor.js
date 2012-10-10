057409c0174efea67362db040c84968e8dd95950
dengen <devnull@localhost>
2012-10-10 22:45:29 +0900

    Fixed JavaScript error in admin/story.php (bug #0001496)

--- public_html/javascript/advanced_editor.js
+++ public_html/javascript/advanced_editor.js
@@ -43,6 +43,17 @@
         f.elements["expire_ampm"].disabled=!disable;
     }
 
+    function enablecmtclose(obj) {
+        var f = obj.form;               // all elements have their parent form in "form"
+        var disable = obj.checked;      // Disable when checked
+        f.elements["cmt_close_month"].disabled=!disable;
+        f.elements["cmt_close_day"].disabled=!disable;
+        f.elements["cmt_close_year"].disabled=!disable;
+        f.elements["cmt_close_hour"].disabled=!disable;
+        f.elements["cmt_close_minute"].disabled=!disable;
+        f.elements["cmt_close_ampm"].disabled=!disable;
+    }
+
     function showhideEditorDiv(option,selindex) {
         var obj = document.getElementById('adveditor');
         var divarray = new Array('publish','images','archive','perms','options','bottom');
