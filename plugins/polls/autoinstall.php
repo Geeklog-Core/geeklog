9a7f463f66fe522047a4ecab97c414e22e57a7cf
Tom <websitemaster@cogeco.net>
2013-04-03 15:18:08 -0400

    Polls Plugin version set to 2.1.5 and Geeklog min version set to 2.0.1

--- plugins/polls/autoinstall.php
+++ plugins/polls/autoinstall.php
@@ -51,8 +51,8 @@ function plugin_autoinstall_polls($pi_name)
     $info = array(
         'pi_name'         => $pi_name,
         'pi_display_name' => $pi_display_name,
-        'pi_version'      => '2.1.4',
-        'pi_gl_version'   => '2.0.0',
+        'pi_version'      => '2.1.5',
+        'pi_gl_version'   => '2.0.1',
         'pi_homepage'     => 'http://www.geeklog.net/'
     );
 
