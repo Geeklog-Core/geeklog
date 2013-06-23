0f2ef647a52d63d6fbfa9017b12cfff230ce31bf
dengen <devnull@localhost>
2013-06-24 01:54:57 +0900

    Optimized COM_getEncodingt function

--- public_html/lib-common.php
+++ public_html/lib-common.php
@@ -8641,32 +8641,22 @@ function COM_newTemplate($root, $options = Array())
 * @return   string      character set, e.g. 'utf-8'
 *
 */
-function COM_getEncodingt() {
-	global $_CONF, $LANG_CHARSET;
+function COM_getEncodingt()
+{
+    static $encoding;
 
-	static $encoding = null;
+    if ($encoding === null) {
+        $encoding = strtolower(COM_getCharset());
+        $valid_charsets = array(
+            'iso-8859-1', 'iso-8859-15', 'utf-8', 'cp866', 'cp1251',
+            'cp1252', 'koi8-r', 'big5', 'gb2312', 'big5-hkscs',
+            'shift_jis', 'sjis', 'euc-jp');
+        if (!in_array($encoding, $valid_charsets)) {
+            $encoding = 'iso-8859-1';
+        }
+    }
 
-    $valid_charsets = array('iso-8859-1','iso-8859-15','utf-8','cp866','cp1251','cp1252','koi8-r','big5','gb2312','big5-hkscs','shift_jis','sjis','euc-jp');
-
-	if ($encoding === null) {
-		if (isset($LANG_CHARSET)) {
-			$encoding = $LANG_CHARSET;
-		} else if (isset($_CONF['default_charset'])) {
-			$encoding = $_CONF['default_charset'];
-		} else {
-			$encoding = 'iso-8859-1';
-		}
-	}
-
-	$encoding = strtolower($encoding);
-
-	if ( in_array($encoding,$valid_charsets) ) {
-	    return $encoding;
-	} else {
-	    return 'iso-8859-1';
-	}
-
-	return $encoding;
+    return $encoding;
 }
 
 /**
