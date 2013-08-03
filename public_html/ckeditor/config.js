e372d174889fc294bd1fbd0fc39eee1c031fc20e
dengen <taharaxp@gmail.com>
2013-08-04 00:01:16 +0900

    Integrated the CKEditor. Leave the FCKEditor yet to do editor switching tests.

---
+++ public_html/ckeditor/config.js
@@ -0,0 +1,79 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+
+	config.protectedSource.push(/<pre[\s\S]+?\/pre>/g);
+	config.protectedSource.push(/\[code[^:][\s\S]+?\/code\]/g);
+	config.protectedSource.push(/\[code:[a-z0-9_]+[\s\S]+?\/code:[a-z0-9_]+\]/g);
+	config.protectedSource.push(/\[raw[\s\S]+?\/raw\]/g);
+	config.protectedSource.push(/\[[a-z0-9_:]+\]/g);
+
+	config.toolbar = 'toolbar1';
+
+	config.toolbar_toolbar1 = [
+		{ name: 'document', items : [ 'Source' ] },
+		{ name: 'clipboard', items : [ 'Undo','Redo' ] },
+		{ name: 'links', items : [ 'Link','Unlink' ] },
+		{ name: 'basicstyles', items : [ 'Bold','Italic' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'align' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight' ] },
+		{ name: 'tools', items : [ 'Maximize','-','About' ] }
+	];
+
+	config.toolbar_toolbar2 = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
+		{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+		{ name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+
+		{ name: 'styles', items: [ 'Styles', 'Format', 'FontSize' ] },
+		{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+		{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
+		{ name: 'about', items: [ 'About' ] }
+	];
+
+	config.toolbar_toolbar3 = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+		{ name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
+		{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+		{ name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+		'/',
+		{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+		{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+		{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
+		{ name: 'others', items: [ '-' ] },
+		{ name: 'about', items: [ 'About' ] }
+	];
+
+	config.toolbar_full = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+		{ name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
+		{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+		{ name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+		'/',
+		{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+		{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+		{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
+		{ name: 'others', items: [ '-' ] },
+		{ name: 'about', items: [ 'About' ] }
+	];
+};
---
+++ public_html/ckeditor/functions.js
@@ -0,0 +1,56 @@
+﻿
+function adve_newEditor(instanceName, options) {
+    switch (options['toolbar']) {
+        case 0:
+            name = 'toolbar1';
+            break;
+        case 1:
+            name = 'toolbar2';
+            break;
+        case 2:
+            name = 'toolbar3';
+            break;
+        default:
+            name = 'full';
+    }
+    CKEDITOR.replace(instanceName, {toolbar:name});
+}
+
+function adve_getContent(instanceName) {
+    return CKEDITOR.instances[instanceName].getData();
+}
+
+function adve_setContent(instanceName, content) {
+    CKEDITOR.instances[instanceName].setData(content);
+}
+
+function adve_changeToolbar(instanceName, toolbar) {
+    var name = '';
+    switch (toolbar) {
+        case 'editor-toolbar1':
+            name = 'toolbar1';
+            break;
+        case 'editor-toolbar2':
+            name = 'toolbar2';
+            break;
+        case 'editor-toolbar3':
+            name = 'toolbar3';
+            break;
+        default:
+            name = 'full';
+    }
+    CKEDITOR.instances[instanceName].destroy();
+    CKEDITOR.replace(instanceName, {toolbar:name});
+}
+
+function adve_changeTextAreaSize(instanceName, option) {
+    var oEditor = CKEDITOR.instances[instanceName];
+    var height = oEditor.ui.space('contents').getStyle('height');
+    currentSize = parseInt(height.replace("px", ""));
+    if (option == 'larger') {
+        var newsize = currentSize + 50;
+    } else if (option == 'smaller') {
+        var newsize = currentSize - 50;
+    }
+    oEditor.resize('100%', newsize, true);
+}
