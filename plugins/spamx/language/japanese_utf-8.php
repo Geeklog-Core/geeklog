f049c8c4f6890f8ffc8815d35789a08ad62ed449
Kenji ITO <mystralkk@gmail.com>
2014-02-02 11:37:41 +0900

    Added Japanese language variables and translations for Spam-X

--- plugins/spamx/language/japanese_utf-8.php
+++ plugins/spamx/language/japanese_utf-8.php
@@ -165,8 +165,10 @@ $LANG_confignames['spamx'] = array(
     'notification_email' => 'メール通知',
     'logging' => 'ログを有効にする',
     'timeout' => 'タイムアウト',
-    'sfs_enabled' => 'Stop Forum Spamモジュールを有効',
-    'snl_enabled' => 'Spam Number of Linksモジュールを有効',
+    'max_age' => 'スパム記録の保存日数',
+    'sfs_enabled' => 'Stop Forum Spamモジュールを有効にする',
+    'sfs_confidence' => '信頼スコアの閾値',
+    'snl_enabled' => 'Spam Number of Linksモジュールを有効にする',
     'snl_num_links' => 'リンクの数'
 );
 
