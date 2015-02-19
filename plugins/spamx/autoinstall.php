d3bac12a7298a53a60917ba182daa2e5032a8615
Tom <devnull@localhost>
2015-02-19 10:13:42 -0500

    Updated Spamx Plugin to Version 1.3.2. Removed unused template.

--- plugins/spamx/autoinstall.php
+++ plugins/spamx/autoinstall.php
@@ -51,7 +51,7 @@ function plugin_autoinstall_spamx($pi_name)
     $info = array(
         'pi_name'         => $pi_name,
         'pi_display_name' => $pi_display_name,
-        'pi_version'      => '1.3.1',
+        'pi_version'      => '1.3.2',
         'pi_gl_version'   => '2.1.0',
         'pi_homepage'     => 'http://www.geeklog.net/'
     );
