501fa84a9434f4c15dd559653ba93b6a8f1106e4
dengenxp <taharaxp@gmail.com>
2016-03-09 20:34:46 +0900

    Removed old header navigation

--- public_html/layout/denim/javascript/script.js
+++ public_html/layout/denim/javascript/script.js
@@ -34,82 +34,7 @@ function postconfirm() {
     return confirm("Send this?");
 }
 
-
-/* http://tinynav.viljamis.com v1.03 by @viljamis */
-/* Modified by dengen */
-/*
-(function ($, window, i) {
-    $.fn.tinyNav = function (options) {
-        // Default settings
-        var settings = $.extend({
-            'active': 'selected',  // String: Set the "active" class
-            'header': true,        // Boolean: Show header instead of the active item
-            'string': 'Navigation' // String: String for header
-        }, options);
-
-        return this.each(function () {
-            // Used for namespacing
-            i++;
-
-            var $nav = $(this),
-                // Namespacing
-                namespace = 'tinynav',
-                namespace_i = namespace + i,
-                l_namespace_i = '.l_' + namespace_i,
-                $select = $('<select/>').addClass(namespace + ' ' + namespace_i);
-
-            if ($nav.is('ul,ol')) {
-                if (settings.header) {
-                    $select.append($('<option/>').text(settings.string));
-                }
-
-                // Build options
-                var options = '';
-
-                $nav
-                    .addClass('l_' + namespace_i)
-                    .find('a')
-                    .each(function () {
-                        options += '<option value="' + $(this).attr('href') + '">';
-
-                        for (j = 0; j < $(this).parents('ul, ol').length - 1; j++) {
-                            options += '&emsp;';
-                        }
-
-                        options += $(this).text() + '</option>';
-                    });
-
-                // Append options into a select
-                $select.append(options);
-
-                // Select the active item
-                if (!settings.header) {
-                    $select
-                        .find(':eq(' + $(l_namespace_i + ' li')
-                            .index($(l_namespace_i + ' li.' + settings.active)) + ')')
-                        .attr('selected', true);
-                }
-
-                // Change window location
-                $select.change(function () {
-                    window.location.href = $(this).val();
-                });
-
-                // Inject select
-                $(l_namespace_i).after($select);
-            }
-        });
-    };
-})(jQuery, this, 0);
-*/
-
 $(function() {
-/*
-    $('#navigation_ul').tinyNav({
-        active: 'selected',
-        string: 'Jump to...'
-    });
-*/
     var istouch = ('ontouchstart' in window);
     var istablet = false;
 
@@ -143,42 +68,6 @@ $(function() {
             });
         }
     }
-/*
-    var iswide = false;
-    var classname1 = 'table-wrapper';
-    var classname2 = 'table-wrapper-fit';
-    var btntext1 = 'Fit';
-    var btntext2 = 'Expand';
-
-    if (istouch && !istablet) {
-        iswide = true;
-    }
-
-    $('.table-wrapper').before('<div class="admin-table-changer">'
-        + '<a class="admin-list-table-changer uk-button uk-button-mini button" href="javascript:void(0);">'
-        + (iswide ? btntext1 : btntext2)
-        + '</a></div>'
-    );
-
-    var tablechanger = $('.admin-list-table-changer');
-
-    if (!iswide) {
-        $('.' + classname1).attr('class', classname2);
-        tablechanger.text(btntext2);
-    }
-
-    $(document).on('click', '.admin-list-table-changer', function () {
-        if (iswide) {
-            $('.' + classname1).attr('class', classname2);
-            tablechanger.text(btntext2);
-        } else {
-            $('.' + classname2).attr('class', classname1);
-            tablechanger.text(btntext1);
-        }
-
-        iswide = !iswide;
-    });
-*/
 
     $('form').addClass('uk-form');
 
