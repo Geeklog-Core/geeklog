5ea16f7430262e16d5eac9d815fa614142321dcf
Kenji ITO <mystralkk@gmail.com>
2016-12-04 16:11:34 +0900

    Updated composer to v1.2.3 and swiftmailer to v5.4.4

--- system/vendor/composer/autoload_files.php
+++ system/vendor/composer/autoload_files.php
@@ -6,6 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '2c102faa651ef8ea5874edb585946bce' => $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
     'ef836782e1b8d747e78815386b148139' => $vendorDir . '/htmlawed/htmlawed/htmLawed.php',
+    '2c102faa651ef8ea5874edb585946bce' => $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
 );
