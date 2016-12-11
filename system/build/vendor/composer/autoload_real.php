db588f0c8cc3d14722f2656af39beed1767eefb3
Kenji ITO <mystralkk@gmail.com>
2016-12-11 16:53:14 +0900

    Updated the build environment

--- system/build/vendor/composer/autoload_real.php
+++ system/build/vendor/composer/autoload_real.php
@@ -27,7 +27,7 @@ class ComposerAutoloaderInit773d23c8c05db964ddef1e220b7111ce
         array_push($includePaths, get_include_path());
         set_include_path(join(PATH_SEPARATOR, $includePaths));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
