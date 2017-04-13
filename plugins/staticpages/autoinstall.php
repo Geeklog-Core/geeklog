8d4490d840d35535a2f5adfb5c48f699a16a700a
Tom Homer <tomhomer@gmail.com>
2017-04-13 11:11:02 -0400

    Fixes for Print Staticpage Page
    Fixes for #777
    - Includes fixes to {page_title} to behave more like page title on
    actual page
    - Update print templates with fixes for link to css and other small
    changes
    - Updated Staticpage version to 1.6.9

--- plugins/staticpages/autoinstall.php
+++ plugins/staticpages/autoinstall.php
@@ -51,7 +51,7 @@ function plugin_autoinstall_staticpages($pi_name)
     $info = array(
         'pi_name'         => $pi_name,
         'pi_display_name' => $pi_display_name,
-        'pi_version'      => '1.6.8',
+        'pi_version'      => '1.6.9',
         'pi_gl_version'   => '2.1.2',
         'pi_homepage'     => 'https://www.geeklog.net/'
     );
