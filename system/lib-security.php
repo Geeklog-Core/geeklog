bffd76e51a2945124e3125e26c7eb8e551a576e3
Tom Homer <tomhomer@gmail.com>
2018-02-11 17:24:15 -0500

    Fixed function exist check for Developer Mode

--- system/lib-security.php
+++ system/lib-security.php
@@ -58,7 +58,7 @@
 // Turn this on to get various debug messages from the code in this library
 // Need to perform check as lib-security is also loaded by the Geeklog Emergency Rescue Tool 
 // which does not load lib-common.php
-if (function_exists(COM_isEnableDeveloperModeLog)) {
+if (function_exists('COM_isEnableDeveloperModeLog')) {
     $_SEC_VERBOSE = COM_isEnableDeveloperModeLog('security');
 } else {
     $_SEC_VERBOSE = false;
