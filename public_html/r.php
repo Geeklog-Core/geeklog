4a12c50531e8230c2abf8c020f453b3c8fc60ea8
Tom Homer <tomhomer@gmail.com>
2018-05-11 14:51:18 -0400

    Resource Class will now gzip output if server and browser supports it
    Resource will always gzip compressed and minified css and js files if server and browser supports it.

--- public_html/r.php
+++ public_html/r.php
@@ -36,6 +36,26 @@ if ($clientETag === $eTag) {
     exit;
 }
 
+
+// Always compress if possible. Don't know $_CONF['compressed_output']) as file does not load lib-common.php
+// Code taken from Geeklog COM_Output function
+$compress_output = false;
+$gzip_accepted = false;
+if (isset($_SERVER['HTTP_ACCEPT_ENCODING'])) {
+    $enc = str_replace(' ', '', $_SERVER['HTTP_ACCEPT_ENCODING']);
+    $accept = explode(',', strtolower($enc));
+    $gzip_accepted = in_array('gzip', $accept);
+}
+
+if ($gzip_accepted && function_exists('gzencode')) {
+    $zlib_comp = ini_get('zlib.output_compression');
+    if (empty($zlib_comp) || (strcasecmp($zlib_comp, 'off') === 0)) {
+        header('Content-encoding: gzip');
+        
+        $compress_output = true;
+    }
+}
+
 // Send correct header type
 switch($data['type']) {
     case 'c':
@@ -55,4 +75,8 @@ switch($data['type']) {
 header('Cache-control: must-revalidate');
 header('Expires: ' . gmdate('D, d M Y H:i:s', time() + Geeklog\Resource::DEFAULT_CACHE_LIFESPAN) . ' GMT');
 header('ETag: "' . $eTag . '"');
-echo $data['data'];
+if ($compress_output) {
+    echo gzencode($data['data']);    
+} else {
+    echo $data['data'];
+}
