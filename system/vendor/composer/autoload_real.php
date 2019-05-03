1b6db3761835a1e682fa84f7ccdca68382b57f4e
mystralkk <mystralkk@gmail.com>
2019-05-03 15:01:41 +0900

    - Updated library classes - Stopped using composer for htmlawed

--- system/vendor/composer/autoload_real.php
+++ system/vendor/composer/autoload_real.php
@@ -24,7 +24,7 @@ class ComposerAutoloaderInite72621cdbb7ab1eae4754470aabe0609
         spl_autoload_unregister(array('ComposerAutoloaderInite72621cdbb7ab1eae4754470aabe0609', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
+        $includePaths[] = get_include_path();
         set_include_path(implode(PATH_SEPARATOR, $includePaths));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
