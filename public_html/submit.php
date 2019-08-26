941fe462fd3d8ca87101825a5da1e7734d2eed26
P2Tera <53715554+P2Tera@users.noreply.github.com>
2019-08-26 22:47:11 +0900

    Update submit.php
    typo?

--- public_html/submit.php
+++ public_html/submit.php
@@ -429,8 +429,8 @@ if (!empty($LANG12[8]) && ($mode === $LANG12[8])) { // submit
     }
 } else {
     if ((strlen($type) > 0) && ($type !== 'story')) {
-        if (SEC_hasRights("$type.edit") ||
-            SEC_hasRights("$type.admin")) {
+        if (SEC_hasRights('type.edit') ||
+            SEC_hasRights('type.admin')) {
             COM_redirect($_CONF['site_admin_url'] . "/plugins/$type/index.php?mode=edit");
         }
     } elseif (SEC_hasRights('story.edit')) {
