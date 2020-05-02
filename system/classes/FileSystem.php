7fe7a715bfdc593afa28504391baa418fbdec697
mystralkk <mystralkk@gmail.com>
2020-05-03 08:40:05 +0900

    Added FileSystem::normalizeFileName to normalize a file name so that it can be safely used on Windows and Unixy systems like Linux

--- system/classes/FileSystem.php
+++ system/classes/FileSystem.php
@@ -7,13 +7,13 @@ namespace Geeklog;
  *
  * @package Geeklog
  */
-class FileSystem
+abstract class FileSystem
 {
     /**
      * Remove file(s) or dir(s)
      *
-     * @param  string $path path to remove
-     * @param  bool $isRecursive
+     * @param  string  $path  path to remove
+     * @param  bool    $isRecursive
      * @return bool true on success
      */
     public static function remove($path, $isRecursive = true)
@@ -21,5 +21,20 @@ class FileSystem
         $flag = $isRecursive ? '-rf ' : '-f ';
 
         return @\System::rm($flag . $path);
+    }
+
+    /**
+     * Normalize a file name so it can safely used both on Windows and Unixy systems like Linux
+     *
+     * @param  string  $fileName
+     * @return string
+     * @see https://en.wikipedia.org/wiki/Filename#Comparison_of_filename_limitations
+     */
+    public static function normalizeFileName($fileName)
+    {
+        $fileName = basename($fileName);
+        $fileName = preg_replace('@[\x00-\x1f\x5c\x7f<>:\"/|?*]@', '_', $fileName);
+
+        return $fileName;
     }
 }
