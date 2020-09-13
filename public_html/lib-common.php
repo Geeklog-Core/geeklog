a03ddde8b1665c8577a0b9232decd25f9b5212f4
eSilverStrike <eSilverStrike@users.noreply.github.com>
2020-09-13 11:14:55 -0400

    Fixes For Migrate in Install
    See #980 for more information.
    
    The install has variable scope issues when lib-common is included. This fixes some issues plugins had during the migration process when calls to SCRIPTS and STRUCT_DATA happens.

--- public_html/lib-common.php
+++ public_html/lib-common.php
@@ -112,14 +112,18 @@ Input::init($_CONF['default_charset']);
 
 if (defined('GL_INSTALL_ACTIVE')) {
     // *********************************************************
-    // IMPORTANT: Have to redeclare the variables below as Global here for Geeklog Install/Upgrade/Migrate.
+    // IMPORTANT: Have to re-declare the variables below as Global here for Geeklog Install/Upgrade/Migrate.
     // For some reason the scope is not correct for these variables when lib-common is included in the Geeklog Install
+	// This also includes scope issues when lib-common adds in plugins functions.inc in install
     // For more info see https://github.com/Geeklog-Core/geeklog/issues/980
     // Once install is fixed this can be removed...
     global $_RIGHTS; // For Geeklog install when lib-common included  SEC_getUserPermissions fails as $_RIGHTS doesn't get loaded
     global $_USER; // For Geeklog install when lib-common included when current user may already be logged an error happens because of $_USER not retrieved when SESS_sessionCheck is called
     global $TEMPLATE_OPTIONS; // For Geeklog install when lib-common included - COM_rdfUpToDateCheck is called when the template class is used an error happens because of $TEMPLATE_OPTIONS being empty.
     global $_TOPICS; // For Geeklog install when lib-common included - TOPIC_getIndex errors as $_TOPIC is not an array
+	global $_SCRIPTS; // For Geeklog install (during Migrate and maybe elsewhere) when lib-common included functions.inc of plugin which called plugin_getheadercode_foo which included a $_SCRIPTS call
+	global $_STRUCT_DATA; // For Geeklog install (during Migrate and maybe elsewhere) when lib-common included functions.inc of plugin and it uses Geeklog's built in Structured Data support where plugin_getstructureddatatypes_foo gets called
+	global $LANG_configselects; // For Geeklog install (during Migrate and maybe elsewhere) when lib-common included functions.inc of forum plugin that modifies this array directly
     // *********************************************************
 }
 
